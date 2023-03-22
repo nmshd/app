3d5e9092855abab12fe9f5b0f082da1c1688a5f3
Julian König <julian.koenig@js-soft.com>
2023-03-22 13:58:56 +0100

    chore: use getIt for DI

--- apps/enmeshed/lib/main.dart
+++ apps/enmeshed/lib/main.dart
@@ -1,20 +1,88 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MainApp());
+import 'package:enmeshed_runtime_bridge/enmeshed_runtime_bridge.dart';
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get_it/get_it.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Permission.camera.request();
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  GetIt.I.registerSingletonAsync<EnmeshedRuntime>(() async => EnmeshedRuntime().run());
+
+  runApp(const EnmeshedApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class EnmeshedApp extends StatelessWidget {
+  const EnmeshedApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: FutureBuilder(
+          future: GetIt.I.allReady(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) return const CircularProgressIndicator();
+
+            return const Scaffold(
+              body: Center(
+                child: Text('The first real Page of your App'),
+              ),
+            );
+          },
         ),
       ),
     );
+  }
+}
+
+class AccountSelectionScreen extends StatelessWidget {
+  const AccountSelectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      builder: (BuildContext context, AsyncSnapshot<List<LocalAccountDTO>> snapshot) {
+        if (!snapshot.hasData) return const CircularProgressIndicator();
+
+        return ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            final item = snapshot.data![index];
+            return ListTile(
+              title: Text(item.name),
+              onTap: () async {
+                GetIt.I.get<EnmeshedRuntime>().selectAccount(item.id);
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (BuildContext context) => AccountScreen(item.id)),
+                );
+              },
+            );
+          },
+          itemCount: snapshot.data!.length,
+        );
+      },
+      future: GetIt.I.get<EnmeshedRuntime>().accountServices.getAccounts(),
+    );
+  }
+}
+
+class AccountScreen extends StatelessWidget {
+  final String accountId;
+
+  const AccountScreen(this.accountId, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final services = GetIt.I.get<EnmeshedRuntime>().getSession(accountId);
+
+    return const Placeholder();
   }
 }
