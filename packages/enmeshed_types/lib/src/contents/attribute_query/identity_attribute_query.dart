fa436a1fa3b454cbf4f91986cc13020947c88fef
Ali Ince <115079695+aince42@users.noreply.github.com>
2023-03-22 17:12:49 +0100

    test/initial types tests (#7)
    * feat(test): indentity attribute query tests
    
    * feat: flutter_types tests
    
    - add validFrom and validTo to abstract class
    - finalize identityAttributeQuery tests
    
    Co-authored-by: Ali Ince <aince42@users.noreply.github.com>
    
    * feat(test): wip relationship_creation_hints
    
    Co-authored-by: Ali Ince <aince42@users.noreply.github.com>
    
    * chore: add mockito and build_runner
    
    * fix: add values to toJson in AttributeQuery
    
    * chore: remove values in toJson in IdentityAttributeQuery
    
    * chore: add props to RelationshipAttributeCreationHints
    
    * chore: add values to fromJson in RelationshipAttributeQuery
    
    * chore: add values to fromJson and add props to ThirdPartyRelationshipAttributeQuery
    
    * test: create tests for AttributeQuery
    
    * fix: fix error in IdentityAttributeQery tests
    
    * test: create tests for RelationshipAttributeCreationHints
    
    * test: create tests for RelationshipAttributeQuery
    
    * test: create tests for ThirdPartyRelationshipAttributeQuery
    
    * chore: add const wherever it is necessary
    
    * chore: generated code from melos
    
    * chore: formatted from melos
    
    * chore: delete unused packages
    
    * chore: refactor and add @mustCallSuper
    
    * fix: fix warnings in get props
    
    * test: refactor code
    
    * test: refactor typo
    
    ---------
    
    Co-authored-by: Siolto <simon.coen@js-soft.com>
    Co-authored-by: Ali Ince <aince42@users.noreply.github.com>

--- packages/enmeshed_types/lib/src/contents/attribute_query/identity_attribute_query.dart
+++ packages/enmeshed_types/lib/src/contents/attribute_query/identity_attribute_query.dart
@@ -4,21 +4,26 @@ class IdentityAttributeQuery extends AttributeQuery {
   final String valueType;
   final List<String>? tags;
 
-  IdentityAttributeQuery({
+  const IdentityAttributeQuery({
     required this.valueType,
     this.tags,
+    super.validFrom,
+    super.validTo,
   });
 
   factory IdentityAttributeQuery.fromJson(Map<String, dynamic> json) {
     return IdentityAttributeQuery(
       valueType: json['valueType'],
       tags: json['tags'] != null ? List<String>.from(json['tags']) : null,
+      validFrom: json['validFrom'],
+      validTo: json['validTo'],
     );
   }
 
   @override
   Map<String, dynamic> toJson() {
     return {
+      ...super.toJson(),
       '@type': 'IdentityAttributeQuery',
       'valueType': valueType,
       if (tags != null) 'tags': tags,
@@ -27,4 +32,11 @@ class IdentityAttributeQuery extends AttributeQuery {
 
   @override
   String toString() => 'IdentityAttributeQuery(valueType: $valueType, tags: $tags)';
+
+  @override
+  List<Object?> get props => [
+        super.props,
+        valueType,
+        tags,
+      ];
 }
---
+++ packages/enmeshed_types/test/contents/attribute_query/third_party_relationship_attribute_query_test.dart
@@ -0,0 +1,84 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Third Party Relationship Attribute Query to json', () {
+    test('valid ThirdPartyRelationshipAttributeQuery', () {
+      const thirdPartyRelationshipAttributeQuery = ThirdPartyRelationshipAttributeQuery(
+        key: 'ProprietaryInteger',
+        owner: 'anOwner',
+        thirdParty: ['anAddress'],
+      );
+      final relationshipJson = thirdPartyRelationshipAttributeQuery.toJson();
+      expect(
+        relationshipJson,
+        equals({
+          '@type': 'ThirdPartyRelationshipAttributeQuery',
+          'key': 'ProprietaryInteger',
+          'owner': 'anOwner',
+          'thirdParty': ['anAddress'],
+        }),
+      );
+    });
+
+    test('valid ThirdPartyRelationshipAttributeQuery with validFrom and validTo', () {
+      const thirdPartyRelationshipAttributeQuery = ThirdPartyRelationshipAttributeQuery(
+        key: 'ProprietaryInteger',
+        owner: 'anOwner',
+        thirdParty: ['anAddress'],
+        validFrom: '1970',
+        validTo: '1980',
+      );
+      final relationshipJson = thirdPartyRelationshipAttributeQuery.toJson();
+      expect(
+        relationshipJson,
+        equals({
+          '@type': 'ThirdPartyRelationshipAttributeQuery',
+          'key': 'ProprietaryInteger',
+          'owner': 'anOwner',
+          'thirdParty': ['anAddress'],
+          'validFrom': '1970',
+          'validTo': '1980',
+        }),
+      );
+    });
+  });
+
+  group('Third Party Relationship Attribute Query from json', () {
+    test('valid ThirdPartyRelationshipAttributeQuery', () {
+      final json = {
+        'key': 'ProprietaryInteger',
+        'owner': 'anOwner',
+        'thirdParty': ['anAddress'],
+      };
+      expect(
+        ThirdPartyRelationshipAttributeQuery.fromJson(json),
+        equals(const ThirdPartyRelationshipAttributeQuery(
+          key: 'ProprietaryInteger',
+          owner: 'anOwner',
+          thirdParty: ['anAddress'],
+        )),
+      );
+    });
+
+    test('valid ThirdPartyRelationshipAttributeQuery with validFrom and validTo', () {
+      final json = {
+        'key': 'ProprietaryInteger',
+        'owner': 'anOwner',
+        'thirdParty': ['anAddress'],
+        'validFrom': '1970',
+        'validTo': '1980',
+      };
+      expect(
+        ThirdPartyRelationshipAttributeQuery.fromJson(json),
+        equals(const ThirdPartyRelationshipAttributeQuery(
+          key: 'ProprietaryInteger',
+          owner: 'anOwner',
+          thirdParty: ['anAddress'],
+          validFrom: '1970',
+          validTo: '1980',
+        )),
+      );
+    });
+  });
+}
