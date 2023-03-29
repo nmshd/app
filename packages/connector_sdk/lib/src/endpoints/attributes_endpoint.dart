169828c5264dd84159ca732a58443542ce40a6f4
Julian König <julian.koenig@js-soft.com>
2023-03-29 12:00:35 +0200

    feat(connector_sdk): implement attributes endpoint

--- packages/connector_sdk/lib/src/endpoints/attributes_endpoint.dart
+++ packages/connector_sdk/lib/src/endpoints/attributes_endpoint.dart
@@ -1,7 +1,58 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+
 import 'endpoint.dart';
+import 'transformers.dart';
 
 class AttributesEndpoint extends Endpoint {
   AttributesEndpoint(super.dio);
 
-  // TODO: implement endpoint
+  Future<ConnectorResponse<LocalAttributeDTO>> createAttribute(AbstractAttribute attribute) => post<LocalAttributeDTO>(
+        '/api/v2/Attributes',
+        data: {
+          'content': attribute.toJson(),
+        },
+        transformer: localAttributeTransformer,
+      );
+
+  Future<ConnectorResponse<List<LocalAttributeDTO>>> getAttributes([Map<String, dynamic>? query]) => get<List<LocalAttributeDTO>>(
+        '/api/v2/Attributes',
+        transformer: localAttributeListTransformer,
+        query: query,
+      );
+
+  Future<ConnectorResponse<LocalAttributeDTO>> getAttribute(String attributeId) => get<LocalAttributeDTO>(
+        '/api/v2/Attributes/$attributeId',
+        transformer: localAttributeTransformer,
+      );
+
+  Future<ConnectorResponse<List<LocalAttributeDTO>>> getValidAttributes([Map<String, dynamic>? query]) => get<List<LocalAttributeDTO>>(
+        '/api/v2/Attributes/Valid',
+        transformer: localAttributeListTransformer,
+        query: query,
+      );
+
+  Future<ConnectorResponse<List<LocalAttributeDTO>>> executeIdentityAttributeQuery(IdentityAttributeQuery query) => post<List<LocalAttributeDTO>>(
+        '/api/v2/Attributes/ExecuteIdentityAttributeQuery',
+        data: {
+          'query': query.toJson(),
+        },
+        transformer: localAttributeListTransformer,
+      );
+
+  Future<ConnectorResponse<LocalAttributeDTO>> executeRelationshipAttributeQuery(RelationshipAttributeQuery query) => post<LocalAttributeDTO>(
+        '/api/v2/Attributes/ExecuteRelationshipAttributeQuery',
+        data: {
+          'query': query.toJson(),
+        },
+        transformer: localAttributeTransformer,
+      );
+
+  Future<ConnectorResponse<List<LocalAttributeDTO>>> executeThirdPartyAttributeQuery(ThirdPartyRelationshipAttributeQuery query) =>
+      post<List<LocalAttributeDTO>>(
+        '/api/v2/Attributes/ExecuteThirdPartyAttributeQuery',
+        data: {
+          'query': query.toJson(),
+        },
+        transformer: localAttributeListTransformer,
+      );
 }
