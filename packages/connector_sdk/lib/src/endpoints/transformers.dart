5e61ea50d728cf220580ecf704092d6dfc061987
Julian König <julian.koenig@js-soft.com>
2023-03-29 10:49:02 +0200

    chore: add transformers

--- packages/connector_sdk/lib/src/endpoints/transformers.dart
+++ packages/connector_sdk/lib/src/endpoints/transformers.dart
@@ -5,3 +5,9 @@ FileDTO fileTransformer(dynamic v) => FileDTO.fromJson(v);
 
 List<TokenDTO> tokenListTransformer(dynamic v) => List<TokenDTO>.from(v.map((e) => TokenDTO.fromJson(e)));
 TokenDTO tokenTransformer(dynamic v) => TokenDTO.fromJson(v);
+
+List<RelationshipDTO> relationshipListTransformer(dynamic v) => List<RelationshipDTO>.from(v.map((e) => RelationshipDTO.fromJson(e)));
+RelationshipDTO relationshipTransformer(dynamic v) => RelationshipDTO.fromJson(v);
+
+List<LocalAttributeDTO> localAttributeListTransformer(dynamic v) => List<LocalAttributeDTO>.from(v.map((e) => LocalAttributeDTO.fromJson(e)));
+LocalAttributeDTO localAttributeTransformer(dynamic v) => LocalAttributeDTO.fromJson(v);
