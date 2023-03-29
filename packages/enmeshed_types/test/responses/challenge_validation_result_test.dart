f7cfe56bdc157f4955e4b2591a048a88665c9c02
Julian König <julian.koenig@js-soft.com>
2023-03-29 16:51:26 +0200

    chore: enhance ChallengeValidationResult test

--- packages/enmeshed_types/test/responses/challenge_validation_result_test.dart
+++ packages/enmeshed_types/test/responses/challenge_validation_result_test.dart
@@ -4,12 +4,41 @@ import 'package:test/test.dart';
 void main() {
   group('ChallengeValidationResult fromJson', () {
     test('is correctly converted', () {
-      // TODO: add test for correspondingRelationship as soon as the RelationshipDTO is tested
-
       final json = {'isValid': true};
       expect(
         ChallengeValidationResult.fromJson(json),
         equals(const ChallengeValidationResult(isValid: true)),
+      );
+    });
+
+    test('is correctly converted with property "correspondingRelationship"', () {
+      final relationshipDTO = RelationshipDTO(
+        id: 'id',
+        template: RelationshipTemplateDTO(
+          id: 'id',
+          isOwn: false,
+          createdBy: 'createdBy',
+          createdByDevice: 'createdByDevice',
+          createdAt: 'createdAt',
+          content: ArbitraryRelationshipTemplateContent({}),
+          truncatedReference: 'truncatedReference',
+        ),
+        status: RelationshipStatus.Active,
+        peer: 'peer',
+        peerIdentity: IdentityDTO(address: 'address', publicKey: 'publicKey', realm: 'realm'),
+        changes: const [],
+      );
+
+      final json = {'isValid': true, 'correspondingRelationship': relationshipDTO.toJson()};
+
+      expect(
+        ChallengeValidationResult.fromJson(json).correspondingRelationship,
+        equals(ChallengeValidationResult(isValid: true, correspondingRelationship: relationshipDTO).correspondingRelationship),
+      );
+
+      expect(
+        ChallengeValidationResult.fromJson(json),
+        equals(ChallengeValidationResult(isValid: true, correspondingRelationship: relationshipDTO)),
       );
     });
   });
