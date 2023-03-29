46437290dd205a5e8a869063d644ca11837d95fa
Julian König <julian.koenig@js-soft.com>
2023-03-29 12:21:15 +0200

    feat(connector_sdk): implement outgoing requests endpoint

--- packages/connector_sdk/lib/src/endpoints/outgoing_requests_endpoint.dart
+++ packages/connector_sdk/lib/src/endpoints/outgoing_requests_endpoint.dart
@@ -1,7 +1,38 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+
 import 'endpoint.dart';
+import 'transformers.dart';
 
 class OutgoingRequestsEndpoint extends Endpoint {
   OutgoingRequestsEndpoint(super.dio);
 
-  // TODO: implement endpoint
+  Future<ConnectorResponse<RequestValidationResultDTO>> canCreateRequest({required Request content, String? peer}) =>
+      post<RequestValidationResultDTO>(
+        '/api/v2/Requests/Outgoing/Validate',
+        transformer: requestValidationResultTransformer,
+        data: {
+          'content': content.toJson(),
+          'peer': peer,
+        },
+      );
+
+  Future<ConnectorResponse<LocalRequestDTO>> createRequest({required Request content, required String peer}) => post<LocalRequestDTO>(
+        '/api/v2/Requests/Outgoing',
+        transformer: localRequestTransformer,
+        data: {
+          'content': content.toJson(),
+          'peer': peer,
+        },
+      );
+
+  Future<ConnectorResponse<LocalRequestDTO>> getRequest(String requestId) => get<LocalRequestDTO>(
+        '/api/v2/Requests/Outgoing/$requestId',
+        transformer: localRequestTransformer,
+      );
+
+  Future<ConnectorResponse<List<LocalRequestDTO>>> getRequests([Map<String, dynamic>? query]) => get<List<LocalRequestDTO>>(
+        '/api/v2/Requests/Outgoing',
+        transformer: localRequestListTransformer,
+        query: query,
+      );
 }
