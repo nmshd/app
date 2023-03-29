697af2e4f15bcbbe029a1c56feda615ac0289a4e
Julian König <julian.koenig@js-soft.com>
2023-03-29 10:48:31 +0200

    feat(connector_sdk): implement monitoring endpoint

--- packages/connector_sdk/lib/src/endpoints/monitoring_endpoint.dart
+++ packages/connector_sdk/lib/src/endpoints/monitoring_endpoint.dart
@@ -3,5 +3,11 @@ import 'endpoint.dart';
 class MonitoringEndpoint extends Endpoint {
   MonitoringEndpoint(super.dio);
 
-  // TODO: implement endpoint
+  Future<Map<String, dynamic>> getHealth() => getPlain<Map<String, dynamic>>('/health');
+
+  Future<Map<String, dynamic>> getVersion() => getPlain<Map<String, dynamic>>('/Monitoring/Version');
+
+  Future<Map<String, dynamic>> getRequests() => getPlain<Map<String, dynamic>>('/Monitoring/Requests');
+
+  Future<Map<String, dynamic>> getSupport() => getPlain<Map<String, dynamic>>('/Monitoring/Support');
 }
