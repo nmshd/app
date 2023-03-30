d84b262a574a995b2b046416851db68bb4d3b63e
Julian König <julian.koenig@js-soft.com>
2023-03-30 18:37:22 +0200

    feat: implement the scaffold of the Connector UI

---
+++ apps/connector_ui/lib/screens/main_screen.dart
@@ -0,0 +1,33 @@
+import 'package:connector_sdk/connector_sdk.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              GetIt.I.unregister<ConnectorClient>();
+              Navigator.of(context).pushReplacementNamed('/login');
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Main Screen'),
+      ),
+    );
+  }
+}
