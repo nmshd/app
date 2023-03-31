c47c15d99c0028f0a697eb7e890e3f8466d4e3f5
Ali Ince <ince.ali@msn.com>
2023-03-31 10:39:39 +0200

    test: add tests for ResponseWrapper

---
+++ packages/enmeshed_types/test/contents/message_content/response_wrapper_test.dart
@@ -0,0 +1,39 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const responseWrapper = ResponseWrapper(
+    requestId: 'aRequestId',
+    requestSourceReference: 'aRequestSourceReference',
+    requestSourceType: RequestSourceType.Message,
+    response: Response(result: 'aResult', requestId: 'aRequestId', items: []),
+  );
+  group('ResponseWrapper toJson', () {
+    test('is correctly converted', () {
+      final identityJson = responseWrapper.toJson();
+      expect(
+        identityJson,
+        equals({
+          '@type': 'ResponseWrapper',
+          'requestId': 'aRequestId',
+          'requestSourceReference': 'aRequestSourceReference',
+          'requestSourceType': RequestSourceType.Message.name,
+          'response': const Response(result: 'aResult', requestId: 'aRequestId', items: []).toJson(),
+        }),
+      );
+    });
+  });
+
+  group('ResponseWrapper fromJson', () {
+    test('is correctly converted', () {
+      final json = {
+        '@type': 'ResponseWrapper',
+        'requestId': 'aRequestId',
+        'requestSourceReference': 'aRequestSourceReference',
+        'requestSourceType': RequestSourceType.Message.name,
+        'response': const Response(result: 'aResult', requestId: 'aRequestId', items: []).toJson(),
+      };
+      expect(ResponseWrapper.fromJson(json), equals(responseWrapper));
+    });
+  });
+}
