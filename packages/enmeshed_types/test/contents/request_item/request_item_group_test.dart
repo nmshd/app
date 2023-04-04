3ed98c55c8dc2deeb1033aa55b934c13b4f6d5ce
Ali Ince <ince.ali@msn.com>
2023-04-04 14:20:21 +0200

    test: add tests for RequestItemGroup

---
+++ packages/enmeshed_types/test/contents/request_item/request_item_group_test.dart
@@ -0,0 +1,134 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('RequestItemGroup toJson', () {
+    test('is correctly converted', () {
+      const requestItemGroup = RequestItemGroup(mustBeAccepted: true, items: []);
+      final requestItemGroupJson = requestItemGroup.toJson();
+      expect(
+        requestItemGroupJson,
+        equals({
+          '@type': 'RequestItemGroup',
+          'mustBeAccepted': true,
+          'items': [],
+        }),
+      );
+    });
+
+    test('is correctly converted with property "title"', () {
+      const requestItemGroup = RequestItemGroup(title: 'aTitle', mustBeAccepted: true, items: []);
+      final requestItemGroupJson = requestItemGroup.toJson();
+      expect(
+        requestItemGroupJson,
+        equals({
+          '@type': 'RequestItemGroup',
+          'title': 'aTitle',
+          'mustBeAccepted': true,
+          'items': [],
+        }),
+      );
+    });
+
+    test('is correctly converted with property "description"', () {
+      const requestItemGroup = RequestItemGroup(description: 'aDescription', mustBeAccepted: true, items: []);
+      final requestItemGroupJson = requestItemGroup.toJson();
+      expect(
+        requestItemGroupJson,
+        equals({
+          '@type': 'RequestItemGroup',
+          'description': 'aDescription',
+          'mustBeAccepted': true,
+          'items': [],
+        }),
+      );
+    });
+
+    test('is correctly converted with property "metadata"', () {
+      const requestItemGroup = RequestItemGroup(metadata: {}, mustBeAccepted: true, items: []);
+      final requestItemGroupJson = requestItemGroup.toJson();
+      expect(
+        requestItemGroupJson,
+        equals({
+          '@type': 'RequestItemGroup',
+          'metadata': {},
+          'mustBeAccepted': true,
+          'items': [],
+        }),
+      );
+    });
+
+    test('is correctly converted with properties "title", "description" and "metadata"', () {
+      const requestItemGroup = RequestItemGroup(title: 'aTitle', description: 'aDescription', metadata: {}, mustBeAccepted: true, items: []);
+      final requestItemGroupJson = requestItemGroup.toJson();
+      expect(
+        requestItemGroupJson,
+        equals({
+          '@type': 'RequestItemGroup',
+          'title': 'aTitle',
+          'description': 'aDescription',
+          'metadata': {},
+          'mustBeAccepted': true,
+          'items': [],
+        }),
+      );
+    });
+  });
+
+  group('RequestItemGroup fromJson', () {
+    test('is correctly converted', () {
+      final json = {
+        'mustBeAccepted': true,
+        'items': [],
+      };
+      expect(RequestItemGroup.fromJson(json), equals(const RequestItemGroup(mustBeAccepted: true, items: [])));
+    });
+
+    test('is correctly converted with property "title', () {
+      final json = {
+        'title': 'aTitle',
+        'mustBeAccepted': true,
+        'items': [],
+      };
+      expect(RequestItemGroup.fromJson(json), equals(const RequestItemGroup(title: 'aTitle', mustBeAccepted: true, items: [])));
+    });
+
+    test('is correctly converted with property "description', () {
+      final json = {
+        'description': 'aDescription',
+        'mustBeAccepted': true,
+        'items': [],
+      };
+      expect(RequestItemGroup.fromJson(json), equals(const RequestItemGroup(description: 'aDescription', mustBeAccepted: true, items: [])));
+    });
+
+    test('is correctly converted with property "metadata', () {
+      final json = {
+        'metadata': {'aKey': 'aValue'},
+        'mustBeAccepted': true,
+        'items': [],
+      };
+      expect(RequestItemGroup.fromJson(json), equals(const RequestItemGroup(metadata: {'aKey': 'aValue'}, mustBeAccepted: true, items: [])));
+    });
+
+    test('is correctly converted with properties "title", "description" and "metadata"', () {
+      final json = {
+        'title': 'aTitle',
+        'description': 'aDescription',
+        'metadata': {'aKey': 'aValue'},
+        'mustBeAccepted': true,
+        'items': [],
+      };
+      expect(
+        RequestItemGroup.fromJson(json),
+        equals(const RequestItemGroup(
+          title: 'aTitle',
+          description: 'aDescription',
+          metadata: {'aKey': 'aValue'},
+          mustBeAccepted: true,
+          items: [],
+        )),
+      );
+    });
+  });
+}
