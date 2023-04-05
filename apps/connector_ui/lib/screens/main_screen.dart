871ee2dfc6b9ac92151794774c422ca515e65b4d
Julian König <julian.koenig@js-soft.com>
2023-04-05 16:23:52 +0200

    chore: simple item rendering

--- apps/connector_ui/lib/screens/main_screen.dart
+++ apps/connector_ui/lib/screens/main_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:connector_sdk/connector_sdk.dart';
+import 'package:enmeshed_types/enmeshed_types.dart' as enmeshed_types;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
@@ -14,20 +16,61 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  List<enmeshed_types.RelationshipDTO> relationships = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _reload();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final client = GetIt.I.get<ConnectorClient>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Main Screen'),
         actions: [
+          IconButton(icon: const Icon(Icons.refresh), onPressed: _reload),
           IconButton(onPressed: _showInfoDialog, icon: const Icon(Icons.info)),
-          const SizedBox(width: 10),
           IconButton(icon: const Icon(Icons.logout), onPressed: _logout),
           const SizedBox(width: 10),
         ],
       ),
-      body: const Center(
-        child: Text('Main Screen'),
+      body: Center(
+        child: ListView.builder(
+          itemBuilder: (context, index) => ExpansionTile(
+            leading: relationships[index].status == enmeshed_types.RelationshipStatus.Active
+                ? const Icon(
+                    Icons.check_circle,
+                    color: Colors.lightGreen,
+                  )
+                : null,
+            title: FutureBuilder(
+              future: renderRelationship(relationships[index]),
+              builder: (context, snapshot) => snapshot.hasData ? Text(snapshot.data.toString()) : Text(relationships[index].peer),
+            ),
+            children: [
+              Text('ID: ${relationships[index].id}'),
+              Text('Peer: ${relationships[index].peer}'),
+              Text('Status: ${relationships[index].status.name}'),
+              ExpansionTile(
+                title: const Text('Attributes'),
+                children: [
+                  FutureBuilder(
+                    future: client.relationships.getAttributesForRelationship(relationships[index].id),
+                    builder: (context, snapshot) => snapshot.hasData
+                        ? Column(children: snapshot.data!.data.map((e) => Text(e.content.toString())).toList())
+                        : const Text('Loading...'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          itemCount: relationships.length,
+        ),
       ),
     );
   }
@@ -104,6 +147,17 @@ class _MainScreenState extends State<MainScreen> {
     }
   }
 
+  void _reload() async {
+    final client = GetIt.I.get<ConnectorClient>();
+
+    await client.account.sync();
+
+    final relationships = (await client.relationships.getRelationships()).data;
+    setState(() {
+      this.relationships = relationships;
+    });
+  }
+
   void _logout() {
     GetIt.I.unregister<ConnectorClient>();
 
@@ -112,5 +166,40 @@ class _MainScreenState extends State<MainScreen> {
     }
 
     Navigator.of(context).pushReplacementNamed('/login');
+  }
+
+  Future<String> renderRelationship(enmeshed_types.RelationshipDTO relationship) async {
+    final client = GetIt.I.get<ConnectorClient>();
+
+    final response = await client.relationships.getAttributesForRelationship(relationship.id);
+    final relationshipAttributes = response.data.where((a) => a.content.owner == relationship.peer);
+
+    final displayName = relationshipAttributes
+        .where(
+          (a) => a.content is enmeshed_types.IdentityAttribute && (a.content as enmeshed_types.IdentityAttribute).value is enmeshed_types.DisplayName,
+        )
+        .firstOrNull;
+    if (displayName != null) {
+      return '${relationship.peer} (${((displayName.content as enmeshed_types.IdentityAttribute).value as enmeshed_types.DisplayName).value})';
+    }
+
+    final surname = relationshipAttributes
+        .where(
+          (a) => a.content is enmeshed_types.IdentityAttribute && (a.content as enmeshed_types.IdentityAttribute).value is enmeshed_types.Surname,
+        )
+        .firstOrNull;
+    final givenName = relationshipAttributes
+        .where(
+          (a) => a.content is enmeshed_types.IdentityAttribute && (a.content as enmeshed_types.IdentityAttribute).value is enmeshed_types.GivenName,
+        )
+        .firstOrNull;
+    if (surname != null || givenName != null) {
+      final name =
+          '${((surname!.content as enmeshed_types.IdentityAttribute).value as enmeshed_types.Surname).value} ${((givenName!.content as enmeshed_types.IdentityAttribute).value as enmeshed_types.GivenName).value}'
+              .trim();
+      return '${relationship.peer} ($name)';
+    }
+
+    return relationship.peer;
   }
 }
