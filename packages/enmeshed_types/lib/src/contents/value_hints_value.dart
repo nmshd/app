a889fd1eb7437c1e50dd05636a41135a725cd131
Ali Ince <ince.ali@msn.com>
2023-04-11 09:47:26 +0200

    chore: add Equatable and its override props

--- packages/enmeshed_types/lib/src/contents/value_hints_value.dart
+++ packages/enmeshed_types/lib/src/contents/value_hints_value.dart
@@ -1,8 +1,10 @@
-class ValueHintsValue {
+import 'package:equatable/equatable.dart';
+
+class ValueHintsValue extends Equatable {
   final dynamic key;
   final String displayName;
 
-  ValueHintsValue({
+  const ValueHintsValue({
     required this.key,
     required this.displayName,
   });
@@ -19,4 +21,7 @@ class ValueHintsValue {
 
   @override
   String toString() => 'ValueHintsValue(key: $key, displayName: $displayName)';
+
+  @override
+  List<Object?> get props => [key, displayName];
 }
