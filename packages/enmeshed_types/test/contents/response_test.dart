4124ae3069b511095967e53f641fc8853d3bd959
Ali Ince <ince.ali@msn.com>
2023-04-11 10:04:38 +0200

    test: add tests for Request

---
+++ packages/enmeshed_types/test/contents/response_test.dart
@@ -0,0 +1,115 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Response toJson', () {
+    test('is correctly converted with an AcceptResponseItem', () {
+      const response = Response(
+        result: 'aResult',
+        requestId: 'aRequestId',
+        items: [
+          ReadAttributeAcceptResponseItem(
+            attributeId: 'anAttributeId',
+            attribute: IdentityAttribute(owner: 'anOwner', value: City(value: 'aCity')),
+          ),
+        ],
+      );
+      final responseJson = response.toJson();
+      expect(
+        responseJson,
+        equals({
+          'result': 'aResult',
+          'requestId': 'aRequestId',
+          'items': [
+            const ReadAttributeAcceptResponseItem(
+              attributeId: 'anAttributeId',
+              attribute: IdentityAttribute(owner: 'anOwner', value: City(value: 'aCity')),
+            ).toJson(),
+          ],
+        }),
+      );
+    });
+
+    test('is correctly converted with a RejectResponseItem', () {
+      const response = Response(
+        result: 'aResult',
+        requestId: 'aRequestId',
+        items: [RejectResponseItem()],
+      );
+      final responseJson = response.toJson();
+      expect(
+        responseJson,
+        equals({
+          'result': 'aResult',
+          'requestId': 'aRequestId',
+          'items': [const RejectResponseItem().toJson()],
+        }),
+      );
+    });
+
+    test('is correctly converted with an ErrorResponseItem', () {
+      const response = Response(
+        result: 'aResult',
+        requestId: 'aRequestId',
+        items: [ErrorResponseItem(code: 'aCode', message: 'aMessage')],
+      );
+      final responseJson = response.toJson();
+      expect(
+        responseJson,
+        equals({
+          'result': 'aResult',
+          'requestId': 'aRequestId',
+          'items': [const ErrorResponseItem(code: 'aCode', message: 'aMessage').toJson()],
+        }),
+      );
+    });
+  });
+
+  group('Response fromJson', () {
+    test('is correctly converted with an AcceptResponseItem', () {
+      final json = {
+        'result': 'aResult',
+        'requestId': 'aRequestId',
+        'items': [
+          const ReadAttributeAcceptResponseItem(
+            attributeId: 'anAttributeId',
+            attribute: IdentityAttribute(owner: 'anOwner', value: City(value: 'aCity')),
+          ).toJson(),
+        ],
+      };
+      expect(
+        Response.fromJson(json),
+        equals(const Response(result: 'aResult', requestId: 'aRequestId', items: [
+          ReadAttributeAcceptResponseItem(
+            attributeId: 'anAttributeId',
+            attribute: IdentityAttribute(owner: 'anOwner', value: City(value: 'aCity')),
+          ),
+        ])),
+      );
+    });
+
+    test('is correctly converted with a RejectResponseItem', () {
+      final json = {
+        'result': 'aResult',
+        'requestId': 'aRequestId',
+        'items': [const RejectResponseItem().toJson()],
+      };
+      expect(
+        Response.fromJson(json),
+        equals(const Response(result: 'aResult', requestId: 'aRequestId', items: [RejectResponseItem()])),
+      );
+    });
+
+    test('is correctly converted with an ErrorResponseItem', () {
+      final json = {
+        'result': 'aResult',
+        'requestId': 'aRequestId',
+        'items': [const ErrorResponseItem(code: 'aCode', message: 'aMessage').toJson()],
+      };
+      expect(
+        Response.fromJson(json),
+        equals(const Response(result: 'aResult', requestId: 'aRequestId', items: [ErrorResponseItem(code: 'aCode', message: 'aMessage')])),
+      );
+    });
+  });
+}
