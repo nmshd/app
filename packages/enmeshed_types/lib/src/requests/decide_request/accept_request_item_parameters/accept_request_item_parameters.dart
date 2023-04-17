9e21548fd9fe75ed42ff8a53978084d7660b1a15
Ali Ince <ince.ali@msn.com>
2023-04-17 13:03:38 +0200

    Revert "chore: move accept request item parameters file"
    This reverts commit b27eef44cf23b4fdfd9cc008fbdb5c88d362a2f0.

--- packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/accept_request_item_parameters.dart
+++ packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/accept_request_item_parameters.dart
@@ -1,11 +1,2 @@
-import 'package:meta/meta.dart';
-
-import '../decide_request_item_parameters.dart';
-
-class AcceptRequestItemParameters extends DecideRequestItemParameters {
-  const AcceptRequestItemParameters();
-
-  @override
-  @mustCallSuper
-  Map<String, dynamic> toJson() => {'accept': true};
-}
+export 'propose_attribute.dart';
+export 'read_attribute.dart';
--- packages/enmeshed_types/lib/src/requests/decide_request/decide_request.dart
+++ packages/enmeshed_types/lib/src/requests/decide_request/decide_request.dart
@@ -1,6 +1,5 @@
+export 'accept_request_item_parameters.dart';
 export 'accept_request_item_parameters/accept_request_item_parameters.dart';
-export 'accept_request_item_parameters/propose_attribute.dart';
-export 'accept_request_item_parameters/read_attribute.dart';
 export 'decide_request_item_group_parameters.dart';
 export 'decide_request_item_parameters.dart';
 export 'decide_request_parameters.dart';
