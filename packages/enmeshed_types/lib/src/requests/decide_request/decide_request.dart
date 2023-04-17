1c4facd2b5782330d0d6a0bc9edf395898ddee35
Ali Ince <ince.ali@msn.com>
2023-04-17 13:34:22 +0200

    chore: change folder name

--- packages/enmeshed_types/lib/src/requests/decide_request/decide_request.dart
+++ packages/enmeshed_types/lib/src/requests/decide_request/decide_request.dart
@@ -1,6 +1,6 @@
-export 'accept_request_item_parameters/accept_request_item_parameters.dart';
-export 'accept_request_item_parameters/propose_attribute.dart';
-export 'accept_request_item_parameters/read_attribute.dart';
+export 'accept_request/accept_request_item_parameters.dart';
+export 'accept_request/propose_attribute.dart';
+export 'accept_request/read_attribute.dart';
 export 'decide_request_item_group_parameters.dart';
 export 'decide_request_item_parameters.dart';
 export 'decide_request_parameters.dart';
