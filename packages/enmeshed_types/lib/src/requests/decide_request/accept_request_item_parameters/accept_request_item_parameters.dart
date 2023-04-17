4f8689f8b1d88d529e412b661b1cbd39e3803ea7
Ali Ince <ince.ali@msn.com>
2023-04-17 12:42:38 +0200

    chore: merge propose attribute files

--- packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/accept_request_item_parameters.dart
+++ packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/accept_request_item_parameters.dart
@@ -1,6 +1,2 @@
 export 'propose_attribute.dart';
-export 'propose_attribute_with_existing_attribute.dart';
-export 'propose_attribute_with_new_attribute.dart';
 export 'read_attribute.dart';
-export 'read_attribute_with_existing_attribute.dart';
-export 'read_attribute_with_new_attribute.dart';
--- packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/propose_attribute.dart
+++ packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/propose_attribute.dart
@@ -1,5 +1,24 @@
+import '../../../contents/abstract_attribute.dart';
 import '../accept_request_item_parameters.dart';
 
 abstract class AcceptProposeAttributeRequestItemParameters extends AcceptRequestItemParameters {
   const AcceptProposeAttributeRequestItemParameters();
+}
+
+class AcceptProposeAttributeRequestItemParametersWithNewAttribute extends AcceptProposeAttributeRequestItemParameters {
+  final AbstractAttribute attribute;
+
+  const AcceptProposeAttributeRequestItemParametersWithNewAttribute({required this.attribute});
+
+  @override
+  Map<String, dynamic> toJson() => {...super.toJson(), 'attribute': attribute.toJson()};
+}
+
+class AcceptProposeAttributeRequestItemParametersWithExistingAttribute extends AcceptProposeAttributeRequestItemParameters {
+  final String attributeId;
+
+  const AcceptProposeAttributeRequestItemParametersWithExistingAttribute({required this.attributeId});
+
+  @override
+  Map<String, dynamic> toJson() => {...super.toJson(), 'attributeId': attributeId};
 }
