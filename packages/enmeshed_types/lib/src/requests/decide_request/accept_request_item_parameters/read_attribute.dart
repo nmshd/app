02ab9ec5c02a36701604a485500699a2af0a9e73
Ali Ince <ince.ali@msn.com>
2023-04-17 12:43:11 +0200

    chore: merge read attribute files

--- packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/read_attribute.dart
+++ packages/enmeshed_types/lib/src/requests/decide_request/accept_request_item_parameters/read_attribute.dart
@@ -1,3 +1,22 @@
+import '../../../contents/abstract_attribute.dart';
 import '../accept_request_item_parameters.dart';
 
 abstract class AcceptReadAttributeRequestItemParameters extends AcceptRequestItemParameters {}
+
+class AcceptReadAttributeRequestItemParametersWithNewAttribute extends AcceptRequestItemParameters {
+  final AbstractAttribute newAttribute;
+
+  const AcceptReadAttributeRequestItemParametersWithNewAttribute({required this.newAttribute});
+
+  @override
+  Map<String, dynamic> toJson() => {...super.toJson(), 'newAttribute': newAttribute.toJson()};
+}
+
+class AcceptReadAttributeRequestItemParametersWithExistingAttribute extends AcceptRequestItemParameters {
+  final String existingAttributeId;
+
+  const AcceptReadAttributeRequestItemParametersWithExistingAttribute({required this.existingAttributeId});
+
+  @override
+  Map<String, dynamic> toJson() => {...super.toJson(), 'existingAttributeId': existingAttributeId};
+}
