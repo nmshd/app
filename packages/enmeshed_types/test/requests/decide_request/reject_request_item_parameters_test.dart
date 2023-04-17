2e6b2d7260d736ebbebf517eec9c885abbbe5895
Ali Ince <ince.ali@msn.com>
2023-04-17 11:58:38 +0200

    chore: outsource decide requests tests to separate files

--- packages/enmeshed_types/test/requests/decide_request/reject_request_item_parameters_test.dart
+++ packages/enmeshed_types/test/requests/decide_request/reject_request_item_parameters_test.dart
@@ -2,44 +2,6 @@ import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('DecideRequestParameters toJson', () {
-    test('is correctly converted', () {
-      const decideRequestParameters = DecideRequestParameters(requestId: 'aRequestId', items: [AcceptRequestItemParameters()]);
-      final json = decideRequestParameters.toJson();
-      expect(
-        json,
-        equals({
-          'requestId': 'aRequestId',
-          'items': [const AcceptRequestItemParameters().toJson()],
-        }),
-      );
-    });
-  });
-
-  group('DecideRequestItemGroupParameters toJson', () {
-    test('is correctly converted', () {
-      const decideRequestItemGroupParameters = DecideRequestItemGroupParameters(items: [AcceptRequestItemParameters()]);
-      final json = decideRequestItemGroupParameters.toJson();
-      expect(
-        json,
-        equals({
-          'items': [const AcceptRequestItemParameters().toJson()],
-        }),
-      );
-    });
-  });
-
-  group('AcceptRequestItemParameters toJson', () {
-    test('is correctly converted', () {
-      const acceptRequestItemParameters = AcceptRequestItemParameters();
-      final json = acceptRequestItemParameters.toJson();
-      expect(
-        json,
-        equals({'accept': true}),
-      );
-    });
-  });
-
   group('RejectRequestItemParameters toJson', () {
     test('is correctly converted', () {
       const rejectRequestItemParameters = RejectRequestItemParameters();
