91b61886a099a9ba2d95ebdf526b2b38ae7cb40e
Ali Ince <ince.ali@msn.com>
2023-05-23 16:42:49 +0200

    chore: add timeout to suite_test

--- packages/enmeshed_runtime_bridge/integration_test/suite_test.dart
+++ packages/enmeshed_runtime_bridge/integration_test/suite_test.dart
@@ -1,5 +1,8 @@
+@Timeout(Duration(minutes: 1))
+
 import 'package:connector_sdk/connector_sdk.dart';
 import 'package:enmeshed_runtime_bridge/enmeshed_runtime_bridge.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'dummy_app.dart' as dummy_app;
