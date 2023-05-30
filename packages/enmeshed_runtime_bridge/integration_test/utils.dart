26b7dbb1d96a67a2f7311f53352638d744b8905f
Ali Ince <ince.ali@msn.com>
2023-05-30 17:21:14 +0200

    feat: add new function getExpiryString()

--- packages/enmeshed_runtime_bridge/integration_test/utils.dart
+++ packages/enmeshed_runtime_bridge/integration_test/utils.dart
@@ -2,6 +2,10 @@ import 'package:connector_sdk/connector_sdk.dart';
 import 'package:enmeshed_runtime_bridge/enmeshed_runtime_bridge.dart';
 import 'package:enmeshed_types/enmeshed_types.dart';
 
+const expiresAtDuration = Duration(hours: 1);
+
+String getExpiryString() => DateTime.now().add(expiresAtDuration).toRuntimeIsoString();
+
 extension ToRuntimeIsoString on DateTime {
   String toRuntimeIsoString() {
     return copyWith(microsecond: 0).toUtc().toIso8601String();
