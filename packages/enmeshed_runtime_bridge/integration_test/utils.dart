1fd1ce7d0e3187f473d88b5c2f5cf4c2dee64229
Ali Ince <ince.ali@msn.com>
2023-06-05 16:14:27 +0200

    feature(test): add some new utils

--- packages/enmeshed_runtime_bridge/integration_test/utils.dart
+++ packages/enmeshed_runtime_bridge/integration_test/utils.dart
@@ -36,7 +36,23 @@ Future<RelationshipDTO> syncUntilHasRelationship(Session session) async {
 
   do {
     final syncResult = await session.transportServices.accounts.syncEverything();
+    if (syncResult.isError) throw Exception(syncResult.error);
     if (syncResult.value.relationships.isNotEmpty) return syncResult.value.relationships.first;
+
+    retries++;
+    await Future.delayed(Duration(seconds: 5 * retries));
+  } while (retries < 10);
+
+  throw Exception('Could not sync until having a relationship');
+}
+
+Future<MessageDTO> syncUntilHasMessage(Session session) async {
+  int retries = 0;
+
+  do {
+    final syncResult = await session.transportServices.accounts.syncEverything();
+    if (syncResult.isError) throw Exception(syncResult.error);
+    if (syncResult.value.messages.isNotEmpty) return syncResult.value.messages.first;
 
     retries++;
     await Future.delayed(Duration(seconds: 5 * retries));
@@ -63,4 +79,130 @@ Future<RelationshipDTO> establishRelationshipAndSync(Session session, ConnectorC
   assert(createRelationshipResult.hasData);
 
   return await syncUntilHasRelationship(session);
+}
+
+Future<RelationshipDTO> ensureActiveRelationship(Session session1, Session session2) async {
+  final session2Address = (await session2.transportServices.accounts.getIdentityInfo()).value.address;
+  List<RelationshipDTO> relationships =
+      (await session1.transportServices.relationships.getRelationships(query: {'peer': QueryValue.string(session2Address)})).value;
+
+  if (relationships.isEmpty) {
+    await establishRelationshipBetweenSessionsAndSync(session1, session2);
+    relationships = (await session1.transportServices.relationships.getRelationships(query: {'peer': QueryValue.string(session2Address)})).value;
+  }
+  if (relationships.first.status == RelationshipStatus.Pending) {
+    final relationship = relationships.first;
+    await session1.transportServices.relationships.acceptRelationshipChange(
+      relationshipId: relationship.id,
+      changeId: relationship.changes.first.id,
+      content: {},
+    );
+    await syncUntilHasRelationship(session2);
+  }
+  return (await session1.transportServices.relationships.getRelationships()).value.first;
+}
+
+Future<RelationshipDTO> establishRelationshipBetweenSessionsAndSync(Session session1, Session session2) async {
+  final createTemplateResult = await session1.transportServices.relationshipTemplates.createOwnRelationshipTemplate(
+    expiresAt: DateTime.now().add(const Duration(minutes: 5)).toRuntimeIsoString(),
+    content: {},
+  );
+  final connectorLoadTemplateResult = await session2.transportServices.relationshipTemplates.loadPeerRelationshipTemplateByReference(
+    reference: createTemplateResult.value.truncatedReference,
+  );
+  assert(connectorLoadTemplateResult.isSuccess);
+
+  final createRelationshipResult = await session2.transportServices.relationships.createRelationship(
+    templateId: connectorLoadTemplateResult.value.id,
+    content: {'a': 'b'},
+  );
+  assert(createRelationshipResult.isSuccess);
+
+  return await syncUntilHasRelationship(session1);
+}
+
+Future<void> shareAndAcceptPeerAttribute(
+  Session sender,
+  Session recipient,
+  String recipientAddress,
+  AbstractAttribute attribute,
+) async {
+  final createdAttributeResult = await sender.consumptionServices.attributes.createAttribute(content: attribute.toJson());
+  final createdAttribute = createdAttributeResult.value;
+
+  final shareAttributeResult = await sender.consumptionServices.attributes.shareAttribute(
+    attributeId: createdAttribute.id,
+    peer: recipientAddress,
+  );
+  final sharedAttribute = shareAttributeResult.value;
+
+  await syncUntilHasMessage(recipient);
+
+  await recipient.consumptionServices.incomingRequests.accept(
+    params: DecideRequestParameters(requestId: sharedAttribute.id, items: [
+      AcceptReadAttributeRequestItemParametersWithNewAttribute(
+        newAttribute: attribute,
+      ),
+    ]),
+  );
+
+  await syncUntilHasMessage(sender);
+}
+
+Future<LocalAttributeDTO> establishSharedAttributeCopy(Session sender, String senderAddress, String peer, AbstractAttribute attribute) async {
+  final createdAttributeResult = await sender.consumptionServices.attributes.createAttribute(content: attribute.toJson());
+  final createdAttribute = createdAttributeResult.value;
+
+  final sharedAttributeResult = await sender.consumptionServices.attributes.createSharedAttributeCopy(
+    attributeId: createdAttribute.id,
+    peer: peer,
+    requestReference: 'REQIDXXXXXXXXXXXXXXX',
+  );
+  final sharedAttribute = sharedAttributeResult.value;
+
+  return sharedAttribute;
+}
+
+Future<void> waitUntilIncomingRequestStatus(Session recipient, String requestId, LocalRequestStatus status) async {
+  int retries = 0;
+
+  do {
+    final syncResult = await recipient.consumptionServices.incomingRequests.getRequest(requestId: requestId);
+    if (syncResult.isError) throw Exception(syncResult.error);
+    if (syncResult.value.status == status) return;
+
+    retries++;
+    await Future.delayed(const Duration(seconds: 5));
+  } while (retries < 10);
+
+  throw Exception('Timeout on waiting until status is DecisionRequired');
+}
+
+Future<void> exchangeAndAcceptRequestByMessage(Session sender, Session recipient, String recipientAddress, Request request) async {
+  final createRequestResult = await sender.consumptionServices.outgoingRequests.create(content: request, peer: recipientAddress);
+  assert(createRequestResult.isSuccess);
+
+  await Future.delayed(const Duration(seconds: 5));
+  await sender.transportServices.messages.sendMessage(recipients: [recipientAddress], content: createRequestResult.value.content.toJson());
+  await syncUntilHasMessage(recipient);
+  await Future.delayed(const Duration(seconds: 5));
+  await waitUntilIncomingRequestStatus(recipient, createRequestResult.value.id, LocalRequestStatus.ManualDecisionRequired);
+
+  final acceptedRequest = await recipient.consumptionServices.incomingRequests.accept(
+    params: DecideRequestParameters(requestId: createRequestResult.value.id, items: [
+      AcceptReadAttributeRequestItemParametersWithNewAttribute(
+        newAttribute: RelationshipAttribute(
+          owner: recipientAddress,
+          value: const ProprietaryStringAttributeValue(title: 'aTitle', value: 'aProprietaryStringValue'),
+          key: 'website',
+          confidentiality: RelationshipAttributeConfidentiality.public,
+        ),
+      ),
+    ]),
+  );
+  assert(acceptedRequest.isSuccess);
+
+  await Future.delayed(const Duration(seconds: 5));
+  await syncUntilHasMessage(sender);
+  await Future.delayed(const Duration(seconds: 5));
 }
