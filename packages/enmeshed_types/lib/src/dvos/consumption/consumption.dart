b2a69bb599770491c67a6db1c3853f81fe5b5738
Julian König <julian.koenig@js-soft.com>
2023-06-19 11:34:15 +0200

    feat: add consumption structure

--- packages/enmeshed_types/lib/src/dvos/consumption/consumption.dart
+++ packages/enmeshed_types/lib/src/dvos/consumption/consumption.dart
@@ -1,2 +1,6 @@
+export 'decidable_request_template_dvos.dart';
 export 'local_attribute_dvo.dart';
+export 'local_attribute_listener_dvo.dart';
 export 'local_request_dvo.dart';
+export 'peer_relationship_template_dvo.dart';
+export 'relationship_setting_dvo.dart';
