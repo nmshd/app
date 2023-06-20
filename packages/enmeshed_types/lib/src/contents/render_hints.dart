044516c548e8ce78a307cdaf724897246d489d1b
Julian König <julian.koenig@js-soft.com>
2023-06-20 15:47:19 +0200

    feat: implement renderhints

---
+++ packages/enmeshed_types/lib/src/contents/render_hints.dart
@@ -0,0 +1,62 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'render_hints.g.dart';
+
+enum RenderHintsTechnicalType {
+  Boolean,
+  Float,
+  Integer,
+  Object,
+  String,
+  Unknown,
+}
+
+enum RenderHintsEditType {
+  InputLike,
+  ButtonLike,
+  RadioButtonLike,
+  SelectLike,
+  SliderLike,
+  Complex,
+  Secret,
+  TextArea,
+  Upload,
+}
+
+enum RenderHintsDataType {
+  Country,
+  DataURL,
+  EMailAddress,
+  HEXColor,
+  Language,
+  PhoneNumber,
+  URL,
+  FileReference,
+  Date,
+  DatePeriod,
+  DateTime,
+  DateTimePeriod,
+  Time,
+  TimePeriod,
+  Day,
+  Month,
+  Year,
+}
+
+@JsonSerializable()
+class RenderHints {
+  final RenderHintsTechnicalType technicalType;
+  final RenderHintsEditType editType;
+  final RenderHintsDataType? dataType;
+  final Map<String, RenderHints>? propertyHints;
+
+  RenderHints({
+    required this.technicalType,
+    required this.editType,
+    this.dataType,
+    this.propertyHints,
+  });
+
+  factory RenderHints.fromJson(Map<String, dynamic> json) => _$RenderHintsFromJson(json);
+  Map<String, dynamic> toJson() => _$RenderHintsToJson(this);
+}
---
+++ packages/enmeshed_types/lib/src/contents/render_hints.g.dart
@@ -0,0 +1,68 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'render_hints.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RenderHints _$RenderHintsFromJson(Map<String, dynamic> json) => RenderHints(
+      technicalType:
+          $enumDecode(_$RenderHintsTechnicalTypeEnumMap, json['technicalType']),
+      editType: $enumDecode(_$RenderHintsEditTypeEnumMap, json['editType']),
+      dataType:
+          $enumDecodeNullable(_$RenderHintsDataTypeEnumMap, json['dataType']),
+      propertyHints: (json['propertyHints'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, RenderHints.fromJson(e as Map<String, dynamic>)),
+      ),
+    );
+
+Map<String, dynamic> _$RenderHintsToJson(RenderHints instance) =>
+    <String, dynamic>{
+      'technicalType':
+          _$RenderHintsTechnicalTypeEnumMap[instance.technicalType]!,
+      'editType': _$RenderHintsEditTypeEnumMap[instance.editType]!,
+      'dataType': _$RenderHintsDataTypeEnumMap[instance.dataType],
+      'propertyHints': instance.propertyHints,
+    };
+
+const _$RenderHintsTechnicalTypeEnumMap = {
+  RenderHintsTechnicalType.Boolean: 'Boolean',
+  RenderHintsTechnicalType.Float: 'Float',
+  RenderHintsTechnicalType.Integer: 'Integer',
+  RenderHintsTechnicalType.Object: 'Object',
+  RenderHintsTechnicalType.String: 'String',
+  RenderHintsTechnicalType.Unknown: 'Unknown',
+};
+
+const _$RenderHintsEditTypeEnumMap = {
+  RenderHintsEditType.InputLike: 'InputLike',
+  RenderHintsEditType.ButtonLike: 'ButtonLike',
+  RenderHintsEditType.RadioButtonLike: 'RadioButtonLike',
+  RenderHintsEditType.SelectLike: 'SelectLike',
+  RenderHintsEditType.SliderLike: 'SliderLike',
+  RenderHintsEditType.Complex: 'Complex',
+  RenderHintsEditType.Secret: 'Secret',
+  RenderHintsEditType.TextArea: 'TextArea',
+  RenderHintsEditType.Upload: 'Upload',
+};
+
+const _$RenderHintsDataTypeEnumMap = {
+  RenderHintsDataType.Country: 'Country',
+  RenderHintsDataType.DataURL: 'DataURL',
+  RenderHintsDataType.EMailAddress: 'EMailAddress',
+  RenderHintsDataType.HEXColor: 'HEXColor',
+  RenderHintsDataType.Language: 'Language',
+  RenderHintsDataType.PhoneNumber: 'PhoneNumber',
+  RenderHintsDataType.URL: 'URL',
+  RenderHintsDataType.FileReference: 'FileReference',
+  RenderHintsDataType.Date: 'Date',
+  RenderHintsDataType.DatePeriod: 'DatePeriod',
+  RenderHintsDataType.DateTime: 'DateTime',
+  RenderHintsDataType.DateTimePeriod: 'DateTimePeriod',
+  RenderHintsDataType.Time: 'Time',
+  RenderHintsDataType.TimePeriod: 'TimePeriod',
+  RenderHintsDataType.Day: 'Day',
+  RenderHintsDataType.Month: 'Month',
+  RenderHintsDataType.Year: 'Year',
+};
