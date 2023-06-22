72348280329cb344c1497345181430f85144c50e
Ali Ince <ince.ali@msn.com>
2023-06-22 10:45:15 +0200

    feat(test): add events

--- packages/enmeshed_runtime_bridge/lib/src/events/events.dart
+++ packages/enmeshed_runtime_bridge/lib/src/events/events.dart
@@ -1,4 +1,6 @@
 export 'event.dart';
+export 'incoming_request_received_event.dart';
 export 'incoming_request_status_changed_event.dart';
 export 'message_sent_event.dart';
+export 'outgoing_request_created_event.dart';
 export 'outgoing_request_status_changed_event.dart';
---
+++ packages/enmeshed_runtime_bridge/lib/src/events/outgoing_request_created_event.dart
@@ -0,0 +1,12 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+
+import 'event.dart';
+
+class OutgoingRequestCreatedEvent extends Event {
+  final LocalRequestDTO data;
+
+  const OutgoingRequestCreatedEvent({
+    required this.data,
+    required super.eventTargetAddress,
+  });
+}
