dd1225b860a08c43634f2f3a1f5fce1b1dc125ba
Julian König <julian.koenig@js-soft.com>
2023-06-29 09:34:21 +0200

    feat: allow access for push functionalities

--- packages/enmeshed_runtime_bridge/natives/src/index.ts
+++ packages/enmeshed_runtime_bridge/natives/src/index.ts
@@ -1,12 +1,25 @@
+import {
+  INativePushNotification,
+  RemoteNotificationEvent,
+  RemoteNotificationRegistrationEvent
+} from "@js-soft/native-abstractions";
 import { AppRuntime } from "@nmshd/app-runtime";
 import { NativeBootstrapper } from "./NativeBootstrapper";
 import { UIBridge } from "./uiBridge";
 
-function registerUIBridge() {
+window.registerUIBridge = function () {
   window.runtime.registerUIBridge(new UIBridge());
-}
+};
 
-window.registerUIBridge = registerUIBridge;
+window.setPushToken = async function (token: string) {
+  window.runtime.nativeEnvironment.configAccess.set("pushToken", token);
+  window.runtime.nativeEnvironment.eventBus.publish(new RemoteNotificationRegistrationEvent(token));
+  window.runtime.nativeEnvironment.configAccess.save();
+};
+
+window.triggerRemoteNotificationEvent = async function (notification: INativePushNotification) {
+  window.runtime.nativeEnvironment.eventBus.publish(new RemoteNotificationEvent(notification));
+};
 
 async function main() {
   const bootstrapper = new NativeBootstrapper();
