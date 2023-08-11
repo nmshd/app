91312a10e8df025d1a78e3f762225fbd2d6b17dd
Julian König <julian.koenig@js-soft.com>
2023-08-11 10:28:37 +0200

    feat: imeplement the devices facade

--- packages/enmeshed_runtime_bridge/lib/src/services/facades/facades.dart
+++ packages/enmeshed_runtime_bridge/lib/src/services/facades/facades.dart
@@ -3,6 +3,7 @@ export 'account_facade.dart';
 export 'anonymous_token_facade.dart';
 export 'attribute_listeners_facade.dart';
 export 'attributes_facade.dart';
+export 'devices_facade.dart';
 export 'files_facade.dart';
 export 'incoming_requests_facade.dart';
 export 'messages_facade.dart';
