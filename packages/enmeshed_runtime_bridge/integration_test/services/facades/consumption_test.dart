b03af038ff37654bba6ff75750f2fa07bdc89130
Julian König <julian.koenig@js-soft.com>
2023-08-17 08:32:29 +0200

    test: add tests for the SettingsFacade

--- packages/enmeshed_runtime_bridge/integration_test/services/facades/consumption_test.dart
+++ packages/enmeshed_runtime_bridge/integration_test/services/facades/consumption_test.dart
@@ -3,10 +3,12 @@ import 'package:enmeshed_runtime_bridge/enmeshed_runtime_bridge.dart';
 import '../../setup.dart';
 import 'consumption/attributes_facade_test.dart' as attributes_facade_test;
 import 'consumption/requests_facade_test.dart' as requests_facade_test;
+import 'consumption/settings_facade_test.dart' as settings_facade_test;
 
 void main() async => run(await setup());
 
 void run(EnmeshedRuntime runtime) async {
   attributes_facade_test.run(runtime);
   requests_facade_test.run(runtime);
+  settings_facade_test.run(runtime);
 }
