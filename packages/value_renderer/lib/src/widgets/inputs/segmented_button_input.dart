dddc7c4afcee1fcbb10fc85252c1a6cb134a6a54
Julian König <julian.koenig@js-soft.com>
2023-09-25 15:34:36 +0200

    refactor: migrate to the translated text package

--- packages/value_renderer/lib/src/widgets/inputs/segmented_button_input.dart
+++ packages/value_renderer/lib/src/widgets/inputs/segmented_button_input.dart
@@ -1,5 +1,6 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
+import 'package:translated_text/translated_text.dart';
 
 import '../../value_renderer.dart';
 import '../utils/utils.dart';
