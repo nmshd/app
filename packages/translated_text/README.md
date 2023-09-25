a524da809dfc367585aa6d12b12c46d86f21c7f3
Julian König <julian.koenig@js-soft.com>
2023-09-25 15:34:13 +0200

    feat: add the translated_text package

---
+++ packages/translated_text/README.md
@@ -0,0 +1,11 @@
+# Value Renderer
+
+A utility package extending text to auto translate `i18n://` prefixed strings using `flutter_i18n`.
+
+## Documentation
+
+You can find the documentation for Enmeshed on [enmeshed.eu](https://enmeshed.eu).
+
+## License
+
+[MIT](LICENSE)
---
+++ packages/translated_text/lib/translated_text.dart
@@ -0,0 +1,49 @@
+library translated_text;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+
+class TranslatedText extends Text {
+  const TranslatedText(
+    super.data, {
+    super.key,
+    super.style,
+    super.strutStyle,
+    super.textAlign,
+    super.textDirection,
+    super.locale,
+    super.softWrap,
+    super.overflow,
+    super.textScaleFactor,
+    super.maxLines,
+    super.semanticsLabel,
+    super.textWidthBasis,
+    super.textHeightBehavior,
+    super.selectionColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    assert(data != null, 'A non-null String must be provided to a TranslatedText widget.');
+
+    final text = data!.startsWith('i18n://') ? FlutterI18n.translate(context, data!.substring(7)) : data!;
+
+    return Text(
+      text,
+      key: key,
+      style: style,
+      strutStyle: strutStyle,
+      textAlign: textAlign,
+      textDirection: textDirection,
+      locale: locale,
+      softWrap: softWrap,
+      overflow: overflow,
+      textScaleFactor: textScaleFactor,
+      maxLines: maxLines,
+      semanticsLabel: semanticsLabel,
+      textWidthBasis: textWidthBasis,
+      textHeightBehavior: textHeightBehavior,
+      selectionColor: selectionColor,
+    );
+  }
+}
