bea52621328f18f4d31f71eee49c6c07716ebbc2
Vinicius Moraes <130673089+Vin-RM@users.noreply.github.com>
2023-10-27 14:18:57 +0100

    feat: add consent and register decidables (#97)
    * feat: add consent and register decidables
    
    * chore: update example types

--- packages/renderers/example/lib/pages/decidable_examples/decidable_consent_request_item_example.dart
+++ packages/renderers/example/lib/pages/decidable_examples/decidable_consent_request_item_example.dart
@@ -1,6 +1,8 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:renderers/renderers.dart';
+import 'package:translated_text/translated_text.dart';
 
 class DecidableConsentRequestItemExample extends StatelessWidget {
   const DecidableConsentRequestItemExample({super.key});
@@ -26,7 +28,7 @@ class DecidableConsentRequestItemExample extends StatelessWidget {
     );
 
     const localRequest = LocalRequestDVO(
-      id: 'a id',
+      id: 'an id',
       name: 'a name',
       type: 'LocalRequestDVO',
       isOwn: false,
@@ -48,9 +50,36 @@ class DecidableConsentRequestItemExample extends StatelessWidget {
       items: [decidableConsentRequestItem],
     );
 
-    return const Padding(
-      padding: EdgeInsets.all(16),
-      child: RequestRenderer(request: localRequest),
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Title:', style: TextStyle(fontWeight: FontWeight.bold)),
+            TranslatedText(localRequest.name),
+            const SizedBox(height: 8),
+            if (localRequest.description != null) ...[
+              const Text('Description:', style: TextStyle(fontWeight: FontWeight.bold)),
+              TranslatedText(localRequest.description!),
+              const SizedBox(height: 8),
+            ],
+            const Text('Request ID:', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(localRequest.id),
+            const SizedBox(height: 8),
+            const Text('Status:', style: TextStyle(fontWeight: FontWeight.bold)),
+            TranslatedText(localRequest.statusText),
+            const SizedBox(height: 8),
+            const Text('Created by:', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(localRequest.createdBy.name),
+            const SizedBox(height: 8),
+            const Text('Created at:', style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(DateFormat('yMd', Localizations.localeOf(context).languageCode).format(DateTime.parse(localRequest.createdAt))),
+            const Divider(),
+            const RequestRenderer(request: localRequest),
+          ],
+        ),
+      ),
     );
   }
 }
