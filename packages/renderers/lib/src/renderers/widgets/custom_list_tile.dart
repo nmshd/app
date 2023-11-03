a70c03a1617ecdfd5168b9a1dbbc0674af48266d
Ali Ince <ince.ali@msn.com>
2023-11-03 13:06:10 +0100

    chore: remove horizontal padding

--- packages/renderers/lib/src/renderers/widgets/custom_list_tile.dart
+++ packages/renderers/lib/src/renderers/widgets/custom_list_tile.dart
@@ -5,34 +5,41 @@ class CustomListTile extends StatelessWidget {
   final String title;
   final String? description;
   final Widget? trailing;
+  final void Function()? onPressed;
 
   const CustomListTile({
     super.key,
     required this.title,
     this.description,
     this.trailing,
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 16, top: 12, bottom: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TranslatedText(title, style: const TextStyle(fontSize: 12, color: Color(0xFF42474E))),
-                if (description != null) ...[
-                  const SizedBox(height: 2),
-                  Text(description!, style: const TextStyle(fontSize: 16)),
-                ]
-              ],
+        Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12, bottom: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TranslatedText(title, style: const TextStyle(fontSize: 12, color: Color(0xFF42474E))),
+                    if (description != null) ...[
+                      const SizedBox(height: 2),
+                      Text(description!, style: const TextStyle(fontSize: 16)),
+                    ]
+                  ],
+                ),
+              ),
             ),
-          ),
+            SizedBox(width: 50, child: trailing ?? IconButton(onPressed: onPressed, icon: const Icon(Icons.chevron_right)))
+          ],
         ),
-        if (trailing != null) SizedBox(width: 96, child: trailing)
+        const Divider(height: 0),
       ],
     );
   }
