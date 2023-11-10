91e47117be0c40f59f0268c2158de1ce652d2843
Nicole Ebken <nicole.ebken@js-soft.com>
2023-11-10 10:35:16 +0100

    refactor: export

--- packages/renderers/example/lib/pages/home_screen.dart
+++ packages/renderers/example/lib/pages/home_screen.dart
@@ -1,15 +1,9 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
-import 'package:example_renderers/pages/decidable_examples/decidable_authentication_request_item_example.dart';
-import 'package:example_renderers/pages/decidable_examples/decidable_share_attribute_request_item_example.dart';
 import 'package:flutter/material.dart';
 
 import 'account_requests/requests_screen.dart';
-import 'create_attribute_request_item_example/identity_attribute_example.dart';
-import 'create_attribute_request_item_example/rejected_create_attribute_request_json_example.dart';
-import 'create_attribute_request_item_example/relationship_attribute_example.dart';
-import 'decidable_examples/decidable_consent_request_item_example.dart';
-import 'decidable_examples/decidable_read_request_item_example.dart';
-import 'decidable_examples/decidable_register_request_item_example.dart';
+import 'create_attribute_request_item_example/create_attribute_request_item_example.dart';
+import 'decidable_examples/decidable_examples.dart';
 import 'item_examples.dart';
 import 'item_group_example.dart';
 import 'read_attribute_request_item_example.dart';
