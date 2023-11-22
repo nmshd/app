4dfe3fd0a74bf069f637d1e8ffe11c73a0a422e6
Ali Ince <ince.ali@msn.com>
2023-11-22 09:27:56 +0100

    chore: ValueRendererListTile - remove shouldTranslate - add padding

--- packages/renderers/lib/src/renderers/widgets/value_renderer_list_tile.dart
+++ packages/renderers/lib/src/renderers/widgets/value_renderer_list_tile.dart
@@ -11,7 +11,6 @@ class ValueRendererListTile extends StatelessWidget {
   final ValueRendererController? controller;
   final Widget? trailing;
   final VoidCallback? onPressed;
-  final bool shouldTranslate;
 
   const ValueRendererListTile({
     super.key,
@@ -23,24 +22,25 @@ class ValueRendererListTile extends StatelessWidget {
     this.controller,
     this.trailing,
     this.onPressed,
-    this.shouldTranslate = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: ValueRenderer(
-            fieldName: fieldName,
-            renderHints: renderHints,
-            valueHints: valueHints,
-            initialValue: initialValue,
-            shouldTranslate: shouldTranslate,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: ValueRenderer(
+              fieldName: fieldName,
+              renderHints: renderHints,
+              valueHints: valueHints,
+              initialValue: initialValue,
+            ),
           ),
-        ),
-        SizedBox(width: 50, child: trailing ?? IconButton(onPressed: onPressed, icon: const Icon(Icons.chevron_right)))
-      ],
+          SizedBox(width: 50, child: trailing ?? IconButton(onPressed: onPressed, icon: const Icon(Icons.chevron_right)))
+        ],
+      ),
     );
   }
 }
