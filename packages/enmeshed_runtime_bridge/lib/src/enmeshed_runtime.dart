2b799760ff205d2cec2f3c9931302b3150772422
Julian König <33655937+jkoenig134@users.noreply.github.com>
2023-11-23 10:50:58 +0100

    Feature/apple push environment (#107)
    * feat: make ios push environment configurable
    
    * refactor: naming
    
    * chore: bump app-runtime
    
    * chore: update windows output path

--- packages/enmeshed_runtime_bridge/lib/src/enmeshed_runtime.dart
+++ packages/enmeshed_runtime_bridge/lib/src/enmeshed_runtime.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -19,6 +20,7 @@ typedef RuntimeConfig = ({
   String clientId,
   String clientSecret,
   String applicationId,
+  bool useAppleSandbox,
 });
 
 class EnmeshedRuntime {
@@ -127,6 +129,7 @@ class EnmeshedRuntime {
       handlerName: 'getDefaultConfig',
       callback: (_) => {
         'applicationId': runtimeConfig.applicationId,
+        if (Platform.isIOS || Platform.isMacOS) 'applePushEnvironment': runtimeConfig.useAppleSandbox ? 'Development' : 'Production',
         'transport': {
           'baseUrl': runtimeConfig.baseUrl,
           'logLevel': 'warn',
