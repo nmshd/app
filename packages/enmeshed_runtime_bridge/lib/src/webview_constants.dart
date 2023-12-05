d95a899644dd56dbae86c62ea963bdb76ee3c712
Julian König <julian.koenig@js-soft.com>
2023-12-05 16:23:45 +0100

    feat: allow more hosts

--- packages/enmeshed_runtime_bridge/lib/src/webview_constants.dart
+++ packages/enmeshed_runtime_bridge/lib/src/webview_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 const _htmlContent =
     """<!DOCTYPE html><html><head><meta http-equiv="Content-Security-Policy"
-  content="default-src 'self' http://*.enmeshed.eu https://*.enmeshed.eu https://*.nbpdev.de https://firebaseinstallations.googleapis.com https://fcmregistrations.googleapis.com mailto: nmshd: data: gap: https://ssl.gstatic.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; frame-src 'self'"
+  content="default-src 'self' http://*.enmeshed.eu https://*.enmeshed.eu https://*.nbpdev.de https://*.meinbildungsraum.de https://*.demo.meinbildungsraum.de https://firebaseinstallations.googleapis.com https://fcmregistrations.googleapis.com mailto: nmshd: data: gap: https://ssl.gstatic.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; frame-src 'self'"
 /></head></html>""";
 
 final initialData = InAppWebViewInitialData(
