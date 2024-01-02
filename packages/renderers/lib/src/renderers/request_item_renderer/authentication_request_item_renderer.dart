18a64e5520299b77f83363f48744ca19a5c3cc59
Julian König <julian.koenig@js-soft.com>
2024-01-02 14:23:52 +0100

    refactor: use CustomListTile in AuthenticationRequestItemRenderer

--- packages/renderers/lib/src/renderers/request_item_renderer/authentication_request_item_renderer.dart
+++ packages/renderers/lib/src/renderers/request_item_renderer/authentication_request_item_renderer.dart
@@ -1,6 +1,8 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_list_tile.dart';
+
 class AuthenticationRequestItemRenderer extends StatelessWidget {
   final AuthenticationRequestItemDVO item;
 
@@ -8,9 +10,9 @@ class AuthenticationRequestItemRenderer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 12),
-      child: Text(item.name, style: const TextStyle(fontSize: 16)),
+    return CustomListTile(
+      title: 'i18n://dvo.requestItem.DecidableAuthenticationRequestItem.name',
+      description: item.name,
     );
   }
 }
