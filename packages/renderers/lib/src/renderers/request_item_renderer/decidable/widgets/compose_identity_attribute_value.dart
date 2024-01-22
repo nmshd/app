2b43543148a7bffb80652d52bd3bd487cb8050b3
Julian König <julian.koenig@js-soft.com>
2024-01-22 10:56:08 +0100

    refactor: make cleaner

--- packages/renderers/lib/src/renderers/request_item_renderer/decidable/widgets/compose_identity_attribute_value.dart
+++ packages/renderers/lib/src/renderers/request_item_renderer/decidable/widgets/compose_identity_attribute_value.dart
@@ -1,68 +1,66 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:value_renderer/value_renderer.dart';
 
-import '../../../widgets/request_renderer_controller.dart';
-
 IdentityAttribute? composeIdentityAttributeValue({
   String? valueType,
   ValueRendererInputValue? inputValue,
   required bool isComplex,
-  required RequestRendererController? controller,
 }) {
-  if (inputValue != null) {
-    if (inputValue is ValueRendererInputValueString && inputValue.value == '') return null;
+  if (inputValue == null) return null;
+  if (inputValue is ValueRendererInputValueString && inputValue.value == '') return null;
 
-    if (isComplex && valueType != 'BirthDate') {
-      final complexInputValue = inputValue as ValueRendererInputValueMap;
+  if (isComplex && valueType == 'BirthDate') {
+    final birthDateInputValue = inputValue as ValueRendererInputValueDateTime;
 
-      final attributeValues = complexInputValue.value.map((key, value) {
-        final attributeValue = switch (value) {
-          final ValueRendererInputValueBool attribute => attribute.value.toString(),
-          final ValueRendererInputValueNum attribute => attribute.value.toString(),
-          final ValueRendererInputValueString attribute => attribute.value,
-          final ValueRendererInputValueDateTime attribute => attribute.value,
-          final ValueRendererInputValueMap attribute => attribute.value,
-          _ => null
-        };
+    return IdentityAttribute(
+      owner: '',
+      value: IdentityAttributeValue.fromJson({
+        '@type': valueType,
+        'day': birthDateInputValue.value.day,
+        'month': birthDateInputValue.value.month,
+        'year': birthDateInputValue.value.year,
+      }),
+    );
+  }
 
-        return MapEntry(key, attributeValue != '' ? attributeValue : null);
-      });
+  if (isComplex) {
+    final complexInputValue = inputValue as ValueRendererInputValueMap;
 
-      try {
-        return IdentityAttribute(
-          owner: '',
-          value: IdentityAttributeValue.fromJson({'@type': valueType, ...attributeValues}),
-        );
-      } catch (e) {
-        return null;
-      }
-    } else if (valueType == 'BirthDate') {
-      final birthDateInputValue = inputValue as ValueRendererInputValueDateTime;
-
-      return IdentityAttribute(
-        owner: '',
-        value: IdentityAttributeValue.fromJson({
-          '@type': valueType,
-          'day': birthDateInputValue.value.day,
-          'month': birthDateInputValue.value.month,
-          'year': birthDateInputValue.value.year,
-        }),
-      );
-    } else {
-      final attributeValue = switch (inputValue) {
-        final ValueRendererInputValueBool attribute => attribute.value.toString(),
-        final ValueRendererInputValueNum attribute => attribute.value.toString(),
+    final attributeValues = complexInputValue.value.map((key, value) {
+      final attributeValue = switch (value) {
+        final ValueRendererInputValueBool attribute => attribute.value,
+        final ValueRendererInputValueNum attribute => attribute.value,
         final ValueRendererInputValueString attribute => attribute.value,
         final ValueRendererInputValueDateTime attribute => attribute.value,
         final ValueRendererInputValueMap attribute => attribute.value,
+        _ => null
       };
 
+      return MapEntry(key, attributeValue != '' ? attributeValue : null);
+    });
+
+    try {
       return IdentityAttribute(
         owner: '',
-        value: IdentityAttributeValue.fromJson({'@type': valueType, 'value': attributeValue}),
+        value: IdentityAttributeValue.fromJson({'@type': valueType, ...attributeValues}),
       );
+    } catch (e) {
+      return null;
     }
   }
 
-  return null;
+  final attributeValue = switch (inputValue) {
+    final ValueRendererInputValueBool attribute => attribute.value,
+    final ValueRendererInputValueNum attribute => attribute.value,
+    final ValueRendererInputValueString attribute => attribute.value,
+    final ValueRendererInputValueMap attribute => attribute.value,
+    _ => null
+  };
+
+  if (attributeValue == null) return null;
+
+  return IdentityAttribute(
+    owner: '',
+    value: IdentityAttributeValue.fromJson({'@type': valueType, 'value': attributeValue}),
+  );
 }
--- packages/renderers/lib/src/renderers/request_item_renderer/decidable/widgets/compose_relationship_attribute_value.dart
+++ packages/renderers/lib/src/renderers/request_item_renderer/decidable/widgets/compose_relationship_attribute_value.dart
@@ -1,65 +1,60 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:value_renderer/value_renderer.dart';
 
-import '../../../widgets/request_renderer_controller.dart';
-
 RelationshipAttribute? composeRelationshipAttributeValue({
   String? valueType,
   ValueRendererInputValue? inputValue,
   required bool isComplex,
-  required RequestRendererController? controller,
   ProcessedRelationshipAttributeQueryDVO? query,
 }) {
-  if (inputValue != null) {
-    if (inputValue is ValueRendererInputValueString && inputValue.value == '') return null;
+  if (inputValue == null) return null;
+  if (inputValue is ValueRendererInputValueString && inputValue.value == '') return null;
 
-    if (isComplex) {
-      final complexInputValue = inputValue as ValueRendererInputValueMap;
+  if (isComplex) {
+    final complexInputValue = inputValue as ValueRendererInputValueMap;
 
-      final attributeValues = complexInputValue.value.map((key, value) {
-        final attributeValue = switch (value) {
-          final ValueRendererInputValueBool attribute => attribute.value.toString(),
-          final ValueRendererInputValueNum attribute => attribute.value.toString(),
-          final ValueRendererInputValueString attribute => attribute.value,
-          final ValueRendererInputValueDateTime attribute => attribute.value,
-          final ValueRendererInputValueMap attribute => attribute.value,
-          _ => null
-        };
-
-        return MapEntry(key, attributeValue != '' ? attributeValue : null);
-      });
-
-      try {
-        return RelationshipAttribute(
-          confidentiality: RelationshipAttributeConfidentiality.values.byName(query?.attributeCreationHints.confidentiality ?? 'public'),
-          key: query?.key ?? '',
-          owner: query?.owner.id ?? '',
-          value: RelationshipAttributeValue.fromJson({'@type': valueType, ...attributeValues}),
-        );
-      } catch (e) {
-        return null;
-      }
-    } else {
-      final attributeValue = switch (inputValue) {
-        final ValueRendererInputValueBool attribute => attribute.value.toString(),
-        final ValueRendererInputValueNum attribute => attribute.value.toString(),
+    final attributeValues = complexInputValue.value.map((key, value) {
+      final attributeValue = switch (value) {
+        final ValueRendererInputValueBool attribute => attribute.value,
+        final ValueRendererInputValueNum attribute => attribute.value,
         final ValueRendererInputValueString attribute => attribute.value,
-        final ValueRendererInputValueDateTime attribute => attribute.value,
         final ValueRendererInputValueMap attribute => attribute.value,
+        _ => null
       };
 
+      return MapEntry(key, attributeValue != '' ? attributeValue : null);
+    });
+
+    try {
       return RelationshipAttribute(
         confidentiality: RelationshipAttributeConfidentiality.values.byName(query?.attributeCreationHints.confidentiality ?? 'public'),
         key: query?.key ?? '',
         owner: query?.owner.id ?? '',
-        value: RelationshipAttributeValue.fromJson({
-          '@type': valueType,
-          'title': query?.attributeCreationHints.title ?? '',
-          'value': attributeValue,
-        }),
+        value: RelationshipAttributeValue.fromJson({'@type': valueType, ...attributeValues}),
       );
+    } catch (e) {
+      return null;
     }
   }
 
-  return null;
+  final attributeValue = switch (inputValue) {
+    final ValueRendererInputValueBool attribute => attribute.value,
+    final ValueRendererInputValueNum attribute => attribute.value,
+    final ValueRendererInputValueString attribute => attribute.value,
+    final ValueRendererInputValueMap attribute => attribute.value,
+    _ => null
+  };
+
+  if (attributeValue == null) return null;
+
+  return RelationshipAttribute(
+    confidentiality: RelationshipAttributeConfidentiality.values.byName(query?.attributeCreationHints.confidentiality ?? 'public'),
+    key: query?.key ?? '',
+    owner: query?.owner.id ?? '',
+    value: RelationshipAttributeValue.fromJson({
+      '@type': valueType,
+      'title': query?.attributeCreationHints.title ?? '',
+      'value': attributeValue,
+    }),
+  );
 }
