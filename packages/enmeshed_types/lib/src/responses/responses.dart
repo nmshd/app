2e5c6d1e3485a6e9b63f1b276a5ce19fd12036e2
Julian König <julian.koenig@js-soft.com>
2024-02-05 12:30:56 +0100

    chore: add succession responses

--- packages/enmeshed_types/lib/src/responses/responses.dart
+++ packages/enmeshed_types/lib/src/responses/responses.dart
@@ -3,6 +3,7 @@ export 'create_qr_code.dart';
 export 'download_file.dart';
 export 'get_identity_info.dart';
 export 'load_item_from_truncated_reference.dart';
+export 'succession.dart';
 export 'sync_everything.dart';
 export 'sync_info.dart';
 export 'sync_info_entry.dart';
