b2c75893ec6ed6faea40d4e4767d508c95094362
Julian König <33655937+jkoenig134@users.noreply.github.com>
2024-02-07 10:13:54 +0100

    Fix/migrate data from old app (#131)
    * feat: add readFileAsBinary to dart
    
    * fix: migrate from old app

--- packages/enmeshed_runtime_bridge/natives/src/DatabaseFactory.ts
+++ packages/enmeshed_runtime_bridge/natives/src/DatabaseFactory.ts
@@ -1,16 +1,22 @@
-import { INativeDatabaseFactory } from "@js-soft/native-abstractions";
+import { ILogger } from "@js-soft/logging-abstractions";
+import { INativeDatabaseFactory, NativeErrorCodes } from "@js-soft/native-abstractions";
+import { Document, deserialize } from "bson";
 import loki from "lokijs";
+import pako from "pako";
 import { FileAccess } from "./FileAccess";
 
 export class DatabaseFactory implements INativeDatabaseFactory {
-  public constructor(private fileAccess: FileAccess) {}
+  public constructor(
+    private fileAccess: FileAccess,
+    private readonly logger: ILogger
+  ) {}
 
   public create(
     name: string,
     options?: Partial<LokiConstructorOptions> & Partial<LokiConfigOptions> & Partial<ThrottledSaveDrainOptions>
   ): Loki {
     return new loki(name, {
-      adapter: new NativeDBPersitenceAdapter(this.fileAccess),
+      adapter: new NativeDBPersitenceAdapter(this.fileAccess, this.logger),
       autosave: true,
       autoload: true,
       autosaveInterval: 1000,
@@ -20,16 +26,30 @@ export class DatabaseFactory implements INativeDatabaseFactory {
 }
 
 export class NativeDBPersitenceAdapter implements LokiPersistenceAdapter {
-  public constructor(private readonly fileAccess: FileAccess) {}
+  public constructor(
+    private readonly fileAccess: FileAccess,
+    private readonly logger: ILogger
+  ) {}
 
   public loadDatabase(dbname: string, callback: (value: any) => void): void {
-    this.fileAccess.readFileAsText(dbname).then((res) => {
-      if (res.isSuccess) {
-        callback(res.value);
-      } else {
-        callback(new Error(res.error.message));
+    this.loadDatabaseAsync(dbname).then((res) => callback(res));
+  }
+
+  private async loadDatabaseAsync(dbname: string): Promise<any> {
+    const metadataExists = await this.fileAccess.existsFile(`${dbname}_metadata`);
+    if (metadataExists.isSuccess && metadataExists.value) {
+      const migrator = new PakoBsonMigrator(this.fileAccess, this.logger);
+      try {
+        await migrator.migrate(dbname);
+      } catch (err) {
+        this.logger.error(err);
       }
-    });
+    }
+
+    const res = await this.fileAccess.readFileAsText(dbname);
+    if (!res.isSuccess) return new Error(res.error.message);
+
+    return res.value;
   }
 
   public deleteDatabase(dbname: string, callback: (err?: Error | null, data?: any) => void): void {
@@ -50,5 +70,83 @@ export class NativeDBPersitenceAdapter implements LokiPersistenceAdapter {
         callback(new Error(res.error.message));
       }
     });
+  }
+}
+
+class PakoBsonMigrator {
+  public constructor(
+    private fileAccess: FileAccess,
+    private readonly logger: ILogger
+  ) {}
+
+  public async migrate(dbname: string): Promise<void> {
+    const metadataPath = `${dbname}_metadata`;
+    const metadataPathResult = await this.fileAccess.existsFile(metadataPath);
+
+    if (metadataPathResult.isError) {
+      const error = `Error in loadDatabase: Unable to load database! Database files not found! Reason: ${metadataPathResult.error}`;
+      this.logger.error(error);
+      throw new Error(error);
+    }
+
+    const filesToBeDeleted: string[] = [metadataPath];
+
+    const dbmeta = await this.loadDatabaseMetadata(metadataPath);
+    const promiseArray: Promise<Document>[] = [];
+    if (dbmeta?.collections) {
+      for (const collection of dbmeta.collections) {
+        const collectionPath = `${dbname}_${collection.name}`;
+        filesToBeDeleted.push(collectionPath);
+        promiseArray.push(this.loadCollectionData(collectionPath));
+      }
+      const collectionDataArray = await Promise.all(promiseArray);
+      dbmeta.collections = collectionDataArray;
+    }
+
+    const result = await this.fileAccess.writeFile(dbname, JSON.stringify(dbmeta));
+    if (result.isError) {
+      const error = `Error in loadDatabase: Unable to load database! Database metadata not writable! Reason: ${result.error}`;
+      this.logger.error(error);
+      throw new Error(error);
+    }
+
+    for (const collection of filesToBeDeleted) {
+      const deleteResult = await this.fileAccess.deleteFile(collection);
+      if (deleteResult.isError) {
+        const error = `Error in loadDatabase: Unable to load database! Database collection not deletable! Reason: ${deleteResult.error}`;
+        this.logger.error(error);
+        throw new Error(error);
+      }
+    }
+  }
+
+  private async loadDatabaseMetadata(metadataPath: string): Promise<any> {
+    const result = await this.fileAccess.readFileAsText(metadataPath);
+    if (result.isSuccess) {
+      if (result.value) return JSON.parse(result.value);
+      return {};
+    } else if (result.error.code === NativeErrorCodes.FILESYSTEM_NOT_FOUND) {
+      // Database does not exist
+    } else {
+      const error = `Unable to load database! Database metadata not readable! Reason: ${result.error}`;
+      this.logger.error(error);
+      throw new Error(error);
+    }
+  }
+
+  private async loadCollectionData(collectionPath: string): Promise<any> {
+    const collectionResult = await this.fileAccess.existsFile(collectionPath);
+    if (collectionResult.isError) {
+      throw new Error(`Unable to load database! Database collection file not found! Reason: ${collectionResult.error}`);
+    }
+
+    const result = await this.fileAccess.readFileAsBinary(collectionPath);
+    if (result.isSuccess) {
+      return deserialize(pako.inflate(result.value));
+    }
+
+    const error = `Unable to load database! Database collection file not readable! Reason: ${result.error}`;
+    this.logger.error(error);
+    throw new Error(error);
   }
 }
