7db890264cb93219fa1929ca9e2abce6cf523b78
Nicole Ebken <nicole.ebken@js-soft.com>
2024-02-15 09:20:05 +0100

    chore: add export

--- packages/renderers/lib/renderers.dart
+++ packages/renderers/lib/renderers.dart
@@ -2,5 +2,6 @@ library renderers;
 
 export 'src/abstract_url_launcher.dart';
 export 'src/attribute/attribute_renderer.dart';
+export 'src/request/request_item_renderer/decidable/widgets/compose_attributes.dart';
 export 'src/request/request_renderer.dart';
 export 'src/request/request_renderer_controller.dart';
