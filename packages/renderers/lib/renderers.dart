be9b7d1ba61feb95028ae232c49136898570dd77
Nicole Ebken <nicole.ebken@js-soft.com>
2024-02-15 09:44:40 +0100

    chore: move to utils

--- packages/renderers/lib/renderers.dart
+++ packages/renderers/lib/renderers.dart
@@ -2,6 +2,6 @@ library renderers;
 
 export 'src/abstract_url_launcher.dart';
 export 'src/attribute/attribute_renderer.dart';
-export 'src/request/request_item_renderer/decidable/widgets/compose_attributes.dart';
 export 'src/request/request_renderer.dart';
 export 'src/request/request_renderer_controller.dart';
+export 'src/utils/compose_attributes.dart';
