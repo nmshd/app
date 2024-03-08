32482f79a1f04141d4314a1bae0d463e783822d5
Julian König <julian.koenig@js-soft.com>
2024-03-08 08:04:52 +0100

    feat: implement file reference renderer

---
+++ packages/value_renderer/lib/src/renderers/file_reference.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/utils.dart';
+import '../value_renderer_controller.dart';
+
+class FileReferenceRenderer extends StatefulWidget {
+  final ValueRendererController? controller;
+  final String? fieldName;
+  final AttributeValue? initialValue;
+  final bool mustBeFilledOut;
+  final ValueHints valueHints;
+  final Future<FileDVO> Function(String) expandFileReference;
+  final Future<FileDVO?> Function() chooseFile;
+
+  const FileReferenceRenderer({
+    super.key,
+    this.controller,
+    this.fieldName,
+    this.initialValue,
+    required this.mustBeFilledOut,
+    required this.valueHints,
+    required this.expandFileReference,
+    required this.chooseFile,
+  });
+
+  @override
+  State<FileReferenceRenderer> createState() => _FileReferenceRendererState();
+}
+
+class _FileReferenceRendererState extends State<FileReferenceRenderer> {
+  FileDVO? selectedFile;
+  bool initialLoadComplete = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.initialValue != null) {
+      final truncatedReference = switch (widget.initialValue) {
+        final IdentityFileReferenceAttributeValue fileReference => fileReference.value,
+        final ProprietaryFileReferenceAttributeValue fileReference => fileReference.value,
+        _ => throw Exception('Invalid file reference type'),
+      };
+
+      widget.expandFileReference(truncatedReference).then((value) {
+        setState(() {
+          selectedFile = value;
+          initialLoadComplete = true;
+        });
+
+        widget.controller?.value = ValueRendererInputValueString(value.truncatedReference);
+      });
+    } else {
+      initialLoadComplete = true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final title = widget.fieldName != null ? Text(widget.fieldName!) : null;
+    final subtitle = !initialLoadComplete
+        ? const CircularProgressIndicator()
+        : selectedFile != null
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(selectedFile!.title),
+                  Text(selectedFile!.filename),
+                ],
+              )
+            // TODO: Localize
+            : const Text('No file selected');
+
+    return ListTile(
+      title: title ?? subtitle,
+      subtitle: title != null ? subtitle : null,
+      trailing: TextButton(
+        // TODO: Localize
+        child: const Text('Datei auswählen'),
+        onPressed: () async {
+          final file = await widget.chooseFile();
+          if (file != null) {
+            setState(() {
+              selectedFile = file;
+            });
+
+            widget.controller?.value = ValueRendererInputValueString(file.truncatedReference);
+          }
+        },
+      ),
+    );
+  }
+}
