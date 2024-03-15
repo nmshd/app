7a0e79ef4430a015a9de398d0ce6a894cb224b99
Nicole Ebken <nicole.ebken@js-soft.com>
2024-03-15 13:58:13 +0100

    feat: return ValueRendererValidationError

--- packages/value_renderer/lib/src/renderers/address.dart
+++ packages/value_renderer/lib/src/renderers/address.dart
@@ -5,50 +5,63 @@ import 'package:value_renderer/src/renderers/complex.dart';
 import 'package:value_renderer/value_renderer.dart';
 
 class AddressRenderer extends StatefulWidget {
-  final ValueRendererController? controller;
   final RenderHints renderHints;
   final ValueHints valueHints;
   final String valueType;
-  final String? translatedText;
   final Future<FileDVO> Function(String) expandFileReference;
   final Future<FileDVO?> Function() chooseFile;
   final void Function(FileDVO) openFileDetails;
+  final ValueRendererController? controller;
+  final String? translatedText;
+  final AttributeValue? initialValue;
 
-  const AddressRenderer(
-      {super.key,
-      this.controller,
-      this.translatedText,
-      required this.renderHints,
-      required this.valueHints,
-      required this.expandFileReference,
-      required this.chooseFile,
-      required this.openFileDetails,
-      required this.valueType});
+  const AddressRenderer({
+    super.key,
+    required this.renderHints,
+    required this.valueHints,
+    required this.valueType,
+    required this.expandFileReference,
+    required this.chooseFile,
+    required this.openFileDetails,
+    this.controller,
+    this.translatedText,
+    this.initialValue,
+  });
 
   @override
   State<AddressRenderer> createState() => _AddressRendererState();
 }
 
 class _AddressRendererState extends State<AddressRenderer> {
-  final Map<String, dynamic> _value = {};
-  Map<String, ValueRendererController>? controllers;
+  Map<String, ValueRendererController>? _controllers;
   IdentityAttribute? _attributeValue;
+
+  final Map<String, dynamic> _value = {};
 
   @override
   void initState() {
     super.initState();
 
     if (widget.controller != null) {
-      controllers = <String, ValueRendererController>{};
+      _controllers = <String, ValueRendererController>{};
 
       for (final key in widget.renderHints.propertyHints!.keys) {
         final controller = ValueRendererController();
-        controllers![key] = controller;
+        _controllers![key] = controller;
 
         controller.addListener(() {
           _value[key] = controller.value;
 
-          if (_value[key] is ValueRendererValidationError) {
+          if (controller.value is ValueRendererValidationError) return;
+
+          _attributeValue = composeIdentityAttributeValue(
+            isComplex: widget.renderHints.editType == RenderHintsEditType.Complex,
+            currentAddress: '',
+            valueType: widget.valueType,
+            inputValue: ValueRendererInputValueMap(Map<String, dynamic>.from(_value)),
+          );
+
+          if (_attributeValue == null) {
             widget.controller!.value = ValueRendererValidationError();
             return;
           }
@@ -56,17 +69,13 @@ class _AddressRendererState extends State<AddressRenderer> {
           widget.controller!.value = ValueRendererInputValueMap(Map<String, dynamic>.from(_value));
         });
       }
-
-      widget.controller?.addListener(() {
-        _validate();
-      });
     }
   }
 
   @override
   void dispose() {
-    if (controllers != null) {
-      for (final controller in controllers!.values) {
+    if (_controllers != null) {
+      for (final controller in _controllers!.values) {
         controller.dispose();
       }
     }
@@ -74,27 +83,12 @@ class _AddressRendererState extends State<AddressRenderer> {
     super.dispose();
   }
 
-  void _validate() {
-    if (widget.controller?.value is ValueRendererValidationError) return;
-
-    _attributeValue = composeIdentityAttributeValue(
-      isComplex: widget.renderHints.editType == RenderHintsEditType.Complex,
-      currentAddress: '',
-      valueType: widget.valueType,
-      inputValue: widget.controller!.value,
-    );
-
-    if (_attributeValue != null) return;
-
-    widget.controller!.value = ValueRendererValidationError();
-  }
-
   @override
   Widget build(BuildContext context) {
     return ComplexItemRenderer(
       renderHints: widget.renderHints,
       controller: widget.controller,
-      controllers: controllers,
+      controllers: _controllers,
       valueHints: widget.valueHints,
       valueType: widget.valueType,
       expandFileReference: widget.expandFileReference,
