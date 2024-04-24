b4a73b31f3164bb063a7a943d0a32df015ca54e4
Julian König <julian.koenig@js-soft.com>
2024-04-24 12:07:18 +0200

    refactor: use new package i18n_translated_text

--- packages/value_renderer/lib/src/inputs/checkbox_input.dart
+++ packages/value_renderer/lib/src/inputs/checkbox_input.dart
@@ -1,6 +1,6 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
-import 'package:translated_text/translated_text.dart';
+import 'package:i18n_translated_text/i18n_translated_text.dart';
 
 import '../utils/utils.dart';
 import '../value_renderer_controller.dart';
