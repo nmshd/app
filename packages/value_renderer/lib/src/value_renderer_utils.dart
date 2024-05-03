3921a2e6a11bcf5817cb480df1fe2b05e9bf45f2
Vinicius <vinicius.moraes@js-soft.com>
2024-05-03 10:27:01 +0100

    chore: add datepicker to exports

--- packages/value_renderer/lib/src/value_renderer_utils.dart
+++ packages/value_renderer/lib/src/value_renderer_utils.dart
@@ -1,3 +1,4 @@
+export 'inputs/datepicker_input/datepicker_form_field.dart';
 export 'value_renderer_controller.dart';
 export 'value_renderer_input_value.dart';
 export 'value_renderer_validation_error.dart';
