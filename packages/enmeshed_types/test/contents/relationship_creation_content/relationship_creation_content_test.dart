9ff7fa3d907b88fe725f99c1ebdfaa883e02b958
Magnus-Kuhn <magnus.kuhn@js-soft.com>
2024-06-10 19:23:23 +0200

    test: new creation content tests

---
+++ packages/enmeshed_types/test/contents/relationship_creation_content/relationship_creation_content_test.dart
@@ -0,0 +1,24 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('RelationshipCreationContent fromJson', () {
+    test('parsed valid RelationshipCreationContent', () {
+      final relationshipCreationContentJson = {
+        '@type': 'RelationshipCreationContent',
+        'response': const Response(result: ResponseResult.Accepted, requestId: 'aRequestId', items: []).toJson(),
+      };
+      final relationshipChangeRequestContent = RelationshipCreationContent.fromJson(relationshipCreationContentJson);
+      expect(relationshipChangeRequestContent, isA<RelationshipCreationContentWithResponse>());
+    });
+
+    test('parsed valid ArbitraryRelationshipCreationContent when not given a Response', () {
+      final arbitraryRelationshipCreationContentJson = {
+        '@type': 'RelationshipCreationContent',
+        'internalJson': 'anInternalJson',
+      };
+      final arbitraryRelationshipCreationContent = RelationshipCreationContent.fromJson(arbitraryRelationshipCreationContentJson);
+      expect(arbitraryRelationshipCreationContent, isA<ArbitraryRelationshipCreationContent>());
+    });
+  });
+}
