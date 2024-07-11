701bb76a7a5d76013a0023f773e36eee17507a5f
Magnus-Kuhn <magnus.kuhn@js-soft.com>
2024-07-11 09:08:07 +0200

    chore: thirdPartyAttrOwner enum

--- packages/enmeshed_types/lib/src/contents/attribute_query/third_party_relationship_attribute_query.dart
+++ packages/enmeshed_types/lib/src/contents/attribute_query/third_party_relationship_attribute_query.dart
@@ -1,9 +1,10 @@
 part of 'attribute_query.dart';
 
-@JsonSerializable(includeIfNull: false)
+enum ThirdPartyRelationshipAttributeQueryOwner { thirdParty, recipient, empty }
+
 class ThirdPartyRelationshipAttributeQuery extends AttributeQuery {
   final String key;
-  final String owner;
+  final ThirdPartyRelationshipAttributeQueryOwner owner;
   final List<String> thirdParty;
   final String? validFrom;
   final String? validTo;
@@ -16,9 +17,32 @@ class ThirdPartyRelationshipAttributeQuery extends AttributeQuery {
     this.validTo,
   });
 
-  factory ThirdPartyRelationshipAttributeQuery.fromJson(Map json) => _$ThirdPartyRelationshipAttributeQueryFromJson(Map<String, dynamic>.from(json));
+  factory ThirdPartyRelationshipAttributeQuery.fromJson(Map json) {
+    return ThirdPartyRelationshipAttributeQuery(
+      key: json['key'],
+      owner: json['owner'] == ''
+          ? ThirdPartyRelationshipAttributeQueryOwner.empty
+          : ThirdPartyRelationshipAttributeQueryOwner.values.byName(json['owner']),
+      thirdParty: (json['thirdParty'] as List<String>).map((e) => e).toList(),
+      validFrom: json['validTo'],
+      validTo: json['validTo'],
+    );
+  }
+
   @override
-  Map<String, dynamic> toJson() => {'@type': 'ThirdPartyRelationshipAttributeQuery', ..._$ThirdPartyRelationshipAttributeQueryToJson(this)};
+  Map<String, dynamic> toJson() {
+    final json = <String, dynamic>{
+      '@type': 'ThirdPartyRelationshipAttributeQuery',
+      'key': key,
+      'owner': owner == ThirdPartyRelationshipAttributeQueryOwner.empty ? '' : owner.name,
+      'thirdParty': thirdParty,
+      'validFrom': validFrom,
+      'validTo': validTo,
+    };
+
+    json.removeWhere((key, value) => value == null);
+    return json;
+  }
 
   @override
   List<Object?> get props => [
