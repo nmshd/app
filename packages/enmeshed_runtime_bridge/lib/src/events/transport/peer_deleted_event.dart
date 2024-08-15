13a24975eaa8ee54d5a16564aae775d326c7d7e8
Magnus-Kuhn <magnus.kuhn@js-soft.com>
2024-08-15 09:20:16 +0200

    feat: add events

---
+++ packages/enmeshed_runtime_bridge/lib/src/events/transport/peer_deleted_event.dart
@@ -0,0 +1,12 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+
+import '../event.dart';
+
+class PeerDeletedEvent extends Event {
+  final RelationshipDTO data;
+
+  const PeerDeletedEvent({
+    required this.data,
+    required super.eventTargetAddress,
+  });
+}
