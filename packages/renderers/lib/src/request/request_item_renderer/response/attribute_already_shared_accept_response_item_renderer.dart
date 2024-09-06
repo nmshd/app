7c6a376550e6b6fdf88f65115379673f4cd04156
Julian König <julian.koenig@js-soft.com>
2024-09-06 10:35:55 +0200

    fix: add new renderers

---
+++ packages/renderers/lib/src/request/request_item_renderer/response/attribute_already_shared_accept_response_item_renderer.dart
@@ -0,0 +1,36 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:flutter/material.dart';
+
+import '/src/attribute/identity_attribute_value_renderer.dart';
+import '/src/attribute/relationship_attribute_value_renderer.dart';
+
+class AttributeAlreadySharedAcceptResponseItemRenderer extends StatelessWidget {
+  final AttributeAlreadySharedAcceptResponseItemDVO item;
+  final Future<FileDVO> Function(String) expandFileReference;
+  final void Function(FileDVO) openFileDetails;
+
+  const AttributeAlreadySharedAcceptResponseItemRenderer({
+    super.key,
+    required this.item,
+    required this.expandFileReference,
+    required this.openFileDetails,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return switch (item.attribute.value) {
+      final IdentityAttributeValue value => IdentityAttributeValueRenderer(
+          value: value,
+          valueHints: item.attribute.valueHints,
+          expandFileReference: expandFileReference,
+          openFileDetails: openFileDetails,
+        ),
+      final RelationshipAttributeValue value => RelationshipAttributeValueRenderer(
+          value: value,
+          expandFileReference: expandFileReference,
+          openFileDetails: openFileDetails,
+        ),
+      _ => throw Exception('Unknown AttributeValue: ${item.attribute.valueType}'),
+    };
+  }
+}
