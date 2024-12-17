81b7e46928ddc0d3a901788f20a005c0052d01f1
Julian König <julian.koenig@js-soft.com>
2024-12-17 16:52:01 +0100

    feat: handle many items

--- apps/enmeshed/lib/profiles/profile/modals/delete_local_data_modal.dart
+++ apps/enmeshed/lib/profiles/profile/modals/delete_local_data_modal.dart
@@ -44,58 +44,72 @@ class _DeleteLocalDataModalState extends State<_DeleteLocalDataModal> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 24, right: 8, top: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(context.l10n.profile_localDeletion_title, style: Theme.of(context).textTheme.titleLarge),
-              IconButton(icon: const Icon(Icons.close), onPressed: context.pop),
-            ],
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height * 0.8),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 24, right: 8, top: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(context.l10n.profile_localDeletion_title, style: Theme.of(context).textTheme.titleLarge),
+                IconButton(icon: const Icon(Icons.close), onPressed: context.pop),
+              ],
+            ),
           ),
-        ),
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: VectorGraphic(loader: AssetBytesLoader('assets/svg/confirm_local_data_deletion.svg'), height: 160),
+          Flexible(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: VectorGraphic(loader: AssetBytesLoader('assets/svg/confirm_local_data_deletion.svg'), height: 160),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+                    child: Text(context.l10n.profile_localDeletion_selectProfiles),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16, left: 24, right: 24),
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        for (final account in widget.accountsInDeletion)
+                          DeletionProfileCard(
+                            accountInDeletion: account,
+                            leading: (widget.accountsInDeletion.length < 2)
+                                ? null
+                                : Checkbox(
+                                    value: _selectedAccounts.contains(account),
+                                    onChanged: (value) => setState(() => _selectedAccounts.toggle(account))),
+                            onTap: () => setState(() => _selectedAccounts.toggle(account)),
+                          ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-          child: Text(context.l10n.profile_localDeletion_selectProfiles),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-          child: Column(
-            spacing: 12,
-            children: [
-              for (final account in widget.accountsInDeletion)
-                DeletionProfileCard(
-                  accountInDeletion: account,
-                  leading: (widget.accountsInDeletion.length < 2)
-                      ? null
-                      : Checkbox(value: _selectedAccounts.contains(account), onChanged: (value) => setState(() => _selectedAccounts.toggle(account))),
-                  onTap: () => setState(() => _selectedAccounts.toggle(account)),
-                ),
-            ],
+          Padding(
+            padding: EdgeInsets.only(left: 24, right: 24, top: 24, bottom: MediaQuery.viewPaddingOf(context).bottom + 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                OutlinedButton(onPressed: context.pop, child: Text(context.l10n.cancel)),
+                Gaps.w8,
+                FilledButton(
+                    onPressed: _selectedAccounts.isNotEmpty ? _delete : null, child: Text(context.l10n.profile_localDeletion_acceptDeletion)),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(left: 24, right: 24, top: 24, bottom: MediaQuery.viewPaddingOf(context).bottom + 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              OutlinedButton(onPressed: context.pop, child: Text(context.l10n.cancel)),
-              Gaps.w8,
-              FilledButton(onPressed: _selectedAccounts.isNotEmpty ? _delete : null, child: Text(context.l10n.profile_localDeletion_acceptDeletion)),
-            ],
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
