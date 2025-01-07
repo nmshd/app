a96d0d1a13ba3b11541b2bc2fc20ea551cfbed70
Ali Ince <ince.ali@msn.com>
2025-01-07 10:06:19 +0100

    refactor: outsource file infos

---
+++ apps/enmeshed/lib/account/my_data/file/widgets/file_info_container.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '/core/constants.dart';
+import '/core/utils/extensions.dart';
+
+class FileInfoContainer extends StatelessWidget {
+  final String createdBy;
+  final String createdAt;
+
+  const FileInfoContainer({required this.createdBy, required this.createdAt, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        borderRadius: BorderRadius.circular(4),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${context.l10n.files_owner}: ',
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+              ),
+              Text(
+                '${context.l10n.files_createdAt}: ',
+                style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+              ),
+            ],
+          ),
+          Gaps.w24,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(context.i18nTranslate(createdBy), style: Theme.of(context).textTheme.bodyMedium),
+              Text(context.i18nTranslate(_formatDate(context, createdAt)), style: Theme.of(context).textTheme.bodyMedium),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _formatDate(BuildContext context, String date) {
+    final locale = Localizations.localeOf(context);
+    final parsedDate = DateTime.parse(date).toLocal();
+    return DateFormat('EEEE, d. MMMM y', locale.toString()).format(parsedDate);
+  }
+}
