516599d22b874268a23a3be9c7a439dfd7c8b6a5
Ali Ince <ince.ali@msn.com>
2025-01-13 13:35:34 +0100

    chore: update ContactDetailHeader - diplay hourglass icon in ContactCircleAvatar if status is pending - support all statuses for ContactStatusText

---
+++ apps/enmeshed/lib/account/contacts/widgets/contact_detail_header.dart
@@ -0,0 +1,49 @@
+import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '/core/core.dart';
+import 'widgets.dart';
+
+class ContactDetailHeader extends StatelessWidget {
+  final IdentityDVO contact;
+
+  const ContactDetailHeader({required this.contact, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        children: [
+          ContactCircleAvatar(
+            contact: contact,
+            radius: 36,
+            color: contact.relationship?.status == RelationshipStatus.Pending ? Theme.of(context).colorScheme.surfaceContainerLow : null,
+            child: contact.relationship?.status == RelationshipStatus.Pending
+                ? Icon(Icons.hourglass_top, color: Theme.of(context).colorScheme.onPrimaryFixedVariant, size: 43.2)
+                : null,
+          ),
+          Gaps.w16,
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(contact.isUnknown ? context.l10n.contacts_unknown : contact.name, style: Theme.of(context).textTheme.titleMedium),
+                if (contact.date != null && contact.relationship?.status != RelationshipStatus.Pending)
+                  Text(
+                    context.l10n.contactDetail_connectedSince(
+                      DateFormat.yMMMd(Localizations.localeOf(context).languageCode).format(DateTime.parse(contact.date!).toLocal()),
+                    ),
+                    style: TextStyle(color: Theme.of(context).colorScheme.onSurfaceVariant),
+                  ),
+                ContactStatusText(contact: contact, openContactRequest: null),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
