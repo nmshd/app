6477ebd9da5c90d0e502f2edb0d392725390fffb
Julian König <julian.koenig@js-soft.com>
2025-01-14 08:40:01 +0100

    fix: remove app id

--- packages/enmeshed_runtime_bridge/lib/src/javascript_handlers.dart
+++ packages/enmeshed_runtime_bridge/lib/src/javascript_handlers.dart
@@ -436,7 +436,7 @@ extension LocalNotifications on InAppWebViewController {
   }
 
   void _addWindowsNotificationsJavaScriptHandlers() {
-    final winNotifyPlugin = WindowsNotification(applicationId: r'{D65231B0-B2F1-4857-A4CE-A8E7C6EA7D27}\WindowsPowerShell\v1.0\powershell.exe');
+    final winNotifyPlugin = WindowsNotification(applicationId: null);
 
     winNotifyPlugin.initNotificationCallBack((details) {
       // TODO: handle notification click
