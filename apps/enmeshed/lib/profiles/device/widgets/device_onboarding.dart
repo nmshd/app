b799cfab23b3d20a9d50fbe0425f90c765d625a3
Julian König <julian.koenig@js-soft.com>
2025-01-21 17:24:54 +0100

    fix: pretty device onboarding

--- apps/enmeshed/lib/profiles/device/widgets/device_onboarding.dart
+++ apps/enmeshed/lib/profiles/device/widgets/device_onboarding.dart
@@ -168,36 +168,34 @@ class _DeviceOnboardingQRCode extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(textAlign: TextAlign.center, context.l10n.devices_code_qrDescription),
-          Gaps.h16,
-          Stack(
-            children: [
-              Center(
-                child: QrImageView(
-                  data: link,
-                  semanticsLabel: context.l10n.devices_code_qrSemanticsLabel,
-                  eyeStyle: QrEyeStyle(
-                    eyeShape: QrEyeShape.square,
-                    color: isExpired ? Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.2) : Theme.of(context).colorScheme.scrim,
+          Expanded(
+            child: Stack(
+              children: [
+                Center(
+                  child: QrImageView(
+                    data: link,
+                    semanticsLabel: context.l10n.devices_code_qrSemanticsLabel,
+                    eyeStyle: QrEyeStyle(
+                      eyeShape: QrEyeShape.square,
+                      color: isExpired ? Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.2) : Theme.of(context).colorScheme.scrim,
+                    ),
+                    dataModuleStyle: QrDataModuleStyle(
+                      dataModuleShape: QrDataModuleShape.square,
+                      color: isExpired ? Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.2) : Theme.of(context).colorScheme.scrim,
+                    ),
+                    size: 200,
                   ),
-                  dataModuleStyle: QrDataModuleStyle(
-                    dataModuleShape: QrDataModuleShape.square,
-                    color: isExpired ? Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.2) : Theme.of(context).colorScheme.scrim,
-                  ),
-                  size: 200,
                 ),
-              ),
-              if (isExpired)
-                SizedBox(
-                  height: 200,
-                  child: Center(
+                if (isExpired)
+                  Center(
                     child: FilledButton.icon(
                       onPressed: getDeviceToken,
                       icon: const Icon(Icons.refresh),
                       label: Text(context.l10n.devices_code_generateQr),
                     ),
                   ),
-                ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
@@ -223,31 +221,31 @@ class _DeviceOnboardingUrl extends StatelessWidget {
         children: [
           Text(textAlign: TextAlign.center, context.l10n.devices_code_urlDescription),
           Gaps.h16,
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-            child: isExpired
-                ? SizedBox(
-                    height: 200,
-                    child: Center(
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+              child: isExpired
+                  ? Center(
                       child: FilledButton.icon(
                         onPressed: getDeviceToken,
                         icon: const Icon(Icons.refresh),
                         label: Text(context.l10n.devices_code_generateUrl),
                       ),
+                    )
+                  : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Gaps.h32,
+                        Text(link),
+                        Gaps.h24,
+                        OutlinedButton.icon(
+                          onPressed: () => Clipboard.setData(ClipboardData(text: link)),
+                          icon: const Icon(Icons.file_copy_outlined),
+                          label: Text(context.l10n.devices_code_copy),
+                        ),
+                      ],
                     ),
-                  )
-                : Column(
-                    children: [
-                      Gaps.h32,
-                      Text(link),
-                      Gaps.h24,
-                      OutlinedButton.icon(
-                        onPressed: () => Clipboard.setData(ClipboardData(text: link)),
-                        icon: const Icon(Icons.file_copy_outlined),
-                        label: Text(context.l10n.devices_code_copy),
-                      ),
-                    ],
-                  ),
+            ),
           ),
         ],
       ),
