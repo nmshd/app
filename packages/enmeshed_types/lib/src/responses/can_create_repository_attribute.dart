d4193fccf425ebda8281836ac45cc88c412ac3da
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-01-29 11:47:00 +0100

    Bump runtime (#414)
    * chore: bump runtime
    
    * chore: bump backbone
    
    * feat: add canCreateRepositoryAttribute
    
    * test: add tests for canCreateRepositoryAttribute
    
    * chore: name
    
    * fix: correct response
    
    * chore: wording
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ packages/enmeshed_types/lib/src/responses/can_create_repository_attribute.dart
@@ -0,0 +1,29 @@
+sealed class CanCreateRepositoryAttributeResponse {
+  final bool isSuccess;
+
+  CanCreateRepositoryAttributeResponse({
+    required this.isSuccess,
+  });
+
+  factory CanCreateRepositoryAttributeResponse.fromJson(Map json) {
+    return switch (json['isSuccess']) {
+      true => CanCreateRepositoryAttributeSuccessResponse(),
+      false => CanCreateRepositoryAttributeFailureResponse(code: json['code'], message: json['message']),
+      _ => throw ArgumentError('Invalid isSuccess value: ${json['isSuccess']}'),
+    };
+  }
+}
+
+class CanCreateRepositoryAttributeSuccessResponse extends CanCreateRepositoryAttributeResponse {
+  CanCreateRepositoryAttributeSuccessResponse() : super(isSuccess: true);
+}
+
+class CanCreateRepositoryAttributeFailureResponse extends CanCreateRepositoryAttributeResponse {
+  final String code;
+  final String message;
+
+  CanCreateRepositoryAttributeFailureResponse({
+    required this.code,
+    required this.message,
+  }) : super(isSuccess: false);
+}
