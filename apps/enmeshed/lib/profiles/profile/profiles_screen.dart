61926a4d8c2ad3a7b2f948ef71a9783aef8df870
Julian König <julian.koenig@js-soft.com>
2025-01-31 15:27:49 +0100

    chore: pretty profiles screen

--- apps/enmeshed/lib/profiles/profile/profiles_screen.dart
+++ apps/enmeshed/lib/profiles/profile/profiles_screen.dart
@@ -147,18 +147,15 @@ class _CurrentProfileHeader extends StatelessWidget {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 24),
-              child: Align(
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(45),
-                  onTap: editProfile,
-                  child: ProfilePicture(
-                    radius: 40,
-                    profileName: selectedAccount.name,
-                    image: selectedAccountProfilePicture != null ? FileImage(selectedAccountProfilePicture!) : null,
-                    decorative: true,
-                  ),
+            Align(
+              child: InkWell(
+                borderRadius: BorderRadius.circular(45),
+                onTap: editProfile,
+                child: ProfilePicture(
+                  radius: 60,
+                  profileName: selectedAccount.name,
+                  image: selectedAccountProfilePicture != null ? FileImage(selectedAccountProfilePicture!) : null,
+                  decorative: true,
                 ),
               ),
             ),
@@ -167,7 +164,7 @@ class _CurrentProfileHeader extends StatelessWidget {
         Container(
           width: double.infinity,
           color: Theme.of(context).colorScheme.secondaryContainer,
-          padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+          padding: const EdgeInsets.all(16),
           child: Column(
             children: [
               Text(
