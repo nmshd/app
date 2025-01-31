2e416cd298603ec3f45a20901e5588ddc4040af3
Nicole Ebken <nicole.ebken@js-soft.com>
2025-01-31 16:29:44 +0100

    chore: open mail

--- apps/enmeshed/lib/error_screen.dart
+++ apps/enmeshed/lib/error_screen.dart
@@ -2,6 +2,7 @@ import 'package:enmeshed/core/utils/extensions.dart';
 import 'package:enmeshed_ui_kit/enmeshed_ui_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:vector_graphics/vector_graphics.dart';
 
 class ErrorScreen extends StatelessWidget {
@@ -9,46 +10,48 @@ class ErrorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final contentColor = Theme.of(context).colorScheme.onSecondaryContainer;
+
     return Scaffold(
       appBar: AppBar(title: Text(context.l10n.home_title), centerTitle: true),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const Align(
               alignment: Alignment.topCenter,
               child: VectorGraphic(loader: AssetBytesLoader('assets/svg/error.svg'), height: 160),
             ),
             Gaps.h32,
-            Text(
-              textAlign: TextAlign.center,
-              context.l10n.error_general,
-            ),
+            Text(textAlign: TextAlign.center, context.l10n.error_general),
             Gaps.h24,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        Text('Supportseite', style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Theme.of(context).primaryColor)),
-                        Icon(Icons.arrow_outward, color: Theme.of(context).primaryColor),
-                      ],
-                    ),
+            FilledButton(
+              onPressed: () {
+                launchUrl(Uri.parse('mailto:enmeshed.support@js-soft.com?subject=Support Anfrage'), mode: LaunchMode.externalApplication);
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Theme.of(context).colorScheme.secondaryContainer),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.support, color: contentColor),
+                  Gaps.w8,
+                  Text(
+                    'Supportseite',
+                    style: TextStyle(color: contentColor),
                   ),
-                ),
-                Gaps.w8,
-                FilledButton(
-                  onPressed: () {
-                    context.go('/splash');
-                  },
-                  child: Text(context.l10n.tryAgain),
-                ),
-              ],
+                ],
+              ),
+            ),
+            Gaps.h16,
+            FilledButton(
+              onPressed: () {
+                context.go('/splash');
+              },
+              child: Text(context.l10n.tryAgain),
             ),
           ],
         ),
