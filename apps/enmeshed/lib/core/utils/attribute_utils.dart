97076d39af0dd8c2bb9a3aa1b962fa607471ed2c
Ali Ince <ince.ali@msn.com>
2025-02-03 13:34:36 +0100

    chore: remove unused function

--- apps/enmeshed/lib/core/utils/attribute_utils.dart
+++ apps/enmeshed/lib/core/utils/attribute_utils.dart
@@ -1,56 +1,5 @@
 import 'package:enmeshed_runtime_bridge/enmeshed_runtime_bridge.dart';
 import 'package:enmeshed_types/enmeshed_types.dart';
-import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:go_router/go_router.dart';
-import 'package:logger/logger.dart';
-
-import 'extensions.dart';
-
-Future<LocalAttributeDTO?> createRepositoryAttribute({
-  required String accountId,
-  required BuildContext context,
-  required IdentityAttributeValue value,
-  required VoidCallback onAttributeCreated,
-  ValueNotifier<bool>? createEnabledNotifier,
-  void Function(String errorCode)? onAttributeCreationFailed,
-  List<String>? tags,
-}) async {
-  if (createEnabledNotifier != null) createEnabledNotifier.value = false;
-
-  final session = GetIt.I.get<EnmeshedRuntime>().getSession(accountId);
-
-  final createAttributeResult = await session.consumptionServices.attributes.createRepositoryAttribute(value: value, tags: tags);
-
-  if (createAttributeResult.isSuccess) {
-    if (context.mounted) context.pop();
-    onAttributeCreated();
-
-    return createAttributeResult.value;
-  }
-
-  GetIt.I.get<Logger>().e('Creating new attribute failed caused by: ${createAttributeResult.error}');
-
-  if (onAttributeCreationFailed != null) {
-    onAttributeCreationFailed(createAttributeResult.error.code);
-  } else {
-    if (context.mounted) {
-      await showDialog<void>(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text(context.l10n.error, style: Theme.of(context).textTheme.titleLarge),
-            content: Text(context.l10n.error_createAttribute),
-          );
-        },
-      );
-
-      if (createEnabledNotifier != null) createEnabledNotifier.value = true;
-    }
-  }
-
-  return null;
-}
 
 final List<String> personalDataInitialAttributeTypes = [
   'HonorificPrefix',
