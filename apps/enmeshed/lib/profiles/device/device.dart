005759fb05e8fbcd61cd546152866a92efb076e6
Julian König <julian.koenig@js-soft.com>
2025-02-20 16:26:45 +0100

    fix: remove export

--- apps/enmeshed/lib/profiles/device/device.dart
+++ apps/enmeshed/lib/profiles/device/device.dart
@@ -1,4 +1,3 @@
 export 'device_detail/device_detail_screen.dart';
 export 'device_detail/widgets/device_detail_widgets.dart';
 export 'devices_overview/devices_screen.dart';
-export 'widgets/device_widgets.dart';
