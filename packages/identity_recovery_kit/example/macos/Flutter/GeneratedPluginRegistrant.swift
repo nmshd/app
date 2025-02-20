89159e9f514bc3361f096772502536f737ebaacb
Julian König <julian.koenig@js-soft.com>
2025-02-20 13:23:08 +0100

    fix: ios build

--- packages/identity_recovery_kit/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/identity_recovery_kit/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,30 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
+import file_selector_macos
+import flutter_inappwebview_macos
+import flutter_local_notifications
+import mobile_scanner
 import open_file_mac
+import package_info_plus
 import path_provider_foundation
+import printing
+import shared_preferences_foundation
+import url_launcher_macos
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
