229883cd185c9796c90540de730c05e6034b7e60
Nicole Ebken <nicole.ebken@js-soft.com>
2025-02-26 09:45:18 +0100

    chore: revert changes

--- apps/enmeshed/lib/core/modals/error_dialog.dart
+++ apps/enmeshed/lib/core/modals/error_dialog.dart
@@ -6,9 +6,8 @@ import '../utils/utils.dart';
 
 class ErrorDialog extends StatelessWidget {
   final String? code;
-  final VoidCallback? onButtonPressed;
 
-  const ErrorDialog({required this.code, this.onButtonPressed, super.key});
+  const ErrorDialog({required this.code, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +15,7 @@ class ErrorDialog extends StatelessWidget {
       icon: _icon(context),
       title: Text(_title(context)),
       content: Text(_content(context), textAlign: TextAlign.center, style: Theme.of(context).textTheme.bodyMedium),
-      actions: <Widget>[FilledButton(onPressed: onButtonPressed ?? context.pop, child: Text(_buttonText(context)))],
+      actions: <Widget>[FilledButton(onPressed: context.pop, child: Text(context.l10n.error_understood))],
       actionsAlignment: MainAxisAlignment.center,
     );
   }
@@ -24,15 +23,7 @@ class ErrorDialog extends StatelessWidget {
   Icon _icon(BuildContext context) => switch (code) {
     'error.recordNotFoundOnScanRecoveryKit' ||
     'error.app-runtime.onboardedAccountAlreadyExists' => Icon(Icons.warning_rounded, color: context.customColors.warning),
-    'error.transport.relationships.relationshipNotYetDecomposedByPeer' ||
-    'error.transport.relationships.relationshipTemplateIsExpired' ||
-    'error.relationshipTemplateProcessedModule.requestExpired' => Icon(Icons.error, color: Theme.of(context).colorScheme.error),
     _ => Icon(Icons.cancel, color: Theme.of(context).colorScheme.error),
-  };
-
-  String _buttonText(BuildContext context) => switch (code) {
-    'error.transport.relationships.relationshipTemplateIsExpired' => context.l10n.error_deleteRequest,
-    _ => context.l10n.error_understood,
   };
 
   String _title(BuildContext context) => switch (code) {
@@ -41,11 +32,6 @@ class ErrorDialog extends StatelessWidget {
     'error.relationshipTemplateProcessedModule.relationshipTemplateProcessingError' => context.l10n.errorDialog_QRCodeProcessingFailed_title,
     'error.recordNotFoundOnScanRecoveryKit' => context.l10n.restoreFromIdentityRecovery_errorTitleOnAlreadyUsedRecoveryKit,
     'error.app-runtime.onboardedAccountAlreadyExists' => context.l10n.restoreFromIdentityRecovery_errorTitleOnExistingProfile,
-    'error.transport.relationships.relationshipNotYetDecomposedByPeer' => context.l10n.errorDialog_relationshipNotYetDecomposedByPeer_title,
-    'error.transport.relationships.activeIdentityDeletionProcessOfOwnerOfRelationshipTemplate' =>
-      context.l10n.errorDialog_activeIdentityDeletionProcessOfOwnerOfRelationshipTemplate_title,
-    'error.transport.relationships.relationshipTemplateIsExpired' => context.l10n.errorDialog_relationshipTemplateIsExpired_title,
-    'error.relationshipTemplateProcessedModule.requestExpired' => context.l10n.errorDialog_requestExpired_title,
     _ => context.l10n.errorDialog_title,
   };
 
@@ -55,11 +41,6 @@ class ErrorDialog extends StatelessWidget {
     'error.relationshipTemplateProcessedModule.relationshipTemplateProcessingError' => context.l10n.errorDialog_QRCodeProcessingFailed_description,
     'error.recordNotFoundOnScanRecoveryKit' => context.l10n.restoreFromIdentityRecovery_errorDescriptionOnAlreadyUsedRecoveryKit,
     'error.app-runtime.onboardedAccountAlreadyExists' => context.l10n.restoreFromIdentityRecovery_errorDescriptionOnExistingProfile,
-    'error.transport.relationships.relationshipNotYetDecomposedByPeer' => context.l10n.errorDialog_relationshipNotYetDecomposedByPeer_description,
-    'error.transport.relationships.activeIdentityDeletionProcessOfOwnerOfRelationshipTemplate' =>
-      context.l10n.errorDialog_activeIdentityDeletionProcessOfOwnerOfRelationshipTemplate_description,
-    'error.transport.relationships.relationshipTemplateIsExpired' => context.l10n.errorDialog_relationshipTemplateIsExpired_description,
-    'error.relationshipTemplateProcessedModule.requestExpired' => context.l10n.errorDialog_requestExpired_description,
     _ => context.l10n.errorDialog_description,
   };
 }
