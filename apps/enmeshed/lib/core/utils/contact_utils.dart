b5dc102450688600e903209089daaf8ef71bd307
nicole-eb <139773901+nicole-eb@users.noreply.github.com>
2025-03-06 14:42:47 +0100

    Check if establishing relationship is possible (#451)
    * chore: can accept relationship
    
    * chore: show dialog
    
    * chore: disable button
    
    * chore: add util
    
    * chore: use util
    
    * chore: use util
    
    * chore: use translations
    
    * chore: show error message
    
    * chore: add error code to route
    
    * fix: translation
    
    * fix: comment out code
    
    * refactor: create error details
    
    * chore: add missing status
    
    * chore: add expired text
    
    * fix: color of tiles
    
    * refactor: move type to sepperate file
    
    * refactor: naming
    
    * refactor: check accept inside of other widget
    
    * chore: update translation
    
    * chore: delete when expired
    
    * fix: add missing error details
    
    * fix: naming
    
    * chore: update func
    
    * chore: check if expired
    
    * chore: translation
    
    * fix: make status nullable
    
    * refactor: return
    
    * chore: add new icon
    
    * chore: move logic to separate widget
    
    * refactor: translation key
    
    * chore: use query
    
    * chore: pop screen
    
    * refactor: wording
    
    * fix: unawaited
    
    * chore: remove error details usage
    
    * chore: set value of _canAcceptRequest
    
    * chore: use new dialog
    
    * fix: color of tile
    
    * chore: show dialog
    
    * fix: remove unused function
    
    * refactor: validateRelationshipCreation
    
    * chore: check if request is for relationship
    
    * chore: revert changes
    
    * chore: make local request source nullable
    
    * chore: show error snackbar
    
    * fix: translation key
    
    * fix: check type
    
    * refactor: check status of request
    
    * chore: move logic to request screen
    
    * refactor: check
    
    * chore: move logic for subtitle
    
    * chore: check relationship
    
    * refactor: simplify boolean null handling
    
    * refactor: simplify boolean null handling
    
    * refactor: pass request to trailing widget
    
    * chore: move widget
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- apps/enmeshed/lib/core/utils/contact_utils.dart
+++ apps/enmeshed/lib/core/utils/contact_utils.dart
@@ -82,7 +82,11 @@ Future<List<IdentityDVO>> getContacts({required Session session}) async {
 Future<List<LocalRequestDVO>> incomingOpenRequestsFromRelationshipTemplate({required Session session}) async {
   final incomingRequestResult = await session.consumptionServices.incomingRequests.getRequests(
     query: {
-      'status': QueryValue.stringList([LocalRequestStatus.DecisionRequired.name, LocalRequestStatus.ManualDecisionRequired.name]),
+      'status': QueryValue.stringList([
+        LocalRequestStatus.DecisionRequired.name,
+        LocalRequestStatus.ManualDecisionRequired.name,
+        LocalRequestStatus.Expired.name,
+      ]),
       'source.type': QueryValue.string(LocalRequestSourceType.RelationshipTemplate.name),
     },
   );
@@ -146,4 +150,22 @@ Future<void> deleteContact({
   }
 
   onContactDeleted();
+}
+
+Future<({bool success, String? errorCode})> validateRelationshipCreation({
+  required String accountId,
+  required Session session,
+  LocalRequestDVO? request,
+}) async {
+  if (request == null || request.peer.hasRelationship || request.source?.type != LocalRequestSourceType.RelationshipTemplate) {
+    return (success: true, errorCode: null);
+  }
+
+  final response = await session.transportServices.relationships.canCreateRelationship(templateId: request.source!.reference);
+
+  if (response.value.isSuccess) return (success: true, errorCode: null);
+
+  final failureResponse = response.value as CanCreateRelationshipFailureResponse;
+
+  return (success: false, errorCode: failureResponse.code);
 }
