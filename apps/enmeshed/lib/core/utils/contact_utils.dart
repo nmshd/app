b27a345302251c9fe338c34694590d482db18095
Julian König <julian.koenig@js-soft.com>
2025-03-06 16:27:03 +0100

    chore: undo

--- apps/enmeshed/lib/core/utils/contact_utils.dart
+++ apps/enmeshed/lib/core/utils/contact_utils.dart
@@ -1,6 +1,6 @@
 import 'package:enmeshed_runtime_bridge/enmeshed_runtime_bridge.dart';
 import 'package:enmeshed_types/enmeshed_types.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
 
