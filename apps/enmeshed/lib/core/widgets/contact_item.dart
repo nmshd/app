a6d12f56c013a861dbd5d04fb22c4cd0998f250c
Ali Ince <115079695+aince42@users.noreply.github.com>
2025-03-06 16:57:41 +0100

    Update relationships screen (#462)
    * refactor: make openContactRequest no more required
    
    * refactor: remove from barrel
    
    * chore: pass correct borderColor and subTitle
    
    * refactor: move type to separate file
    
    * refactor: move type to separate file
    
    * feat: add new function to contact_utils
    
    * chore: add export
    
    * chore: simplify logic of subTitle
    
    * chore: pass correct borderColor
    
    * feat: display borderColor if needed
    
    * chore: add translation
    
    * fix: file name
    
    * refactor: rename subtitle
    
    * refactor: call function in-place
    
    * refactor: simplify code
    
    * fix: check for request not finished
    
    * refactor: make understandable
    
    * fix: analyzer
    
    * refactor: simplify stuff
    
    * chore: undo
    
    * fix: function call
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Julian König <julian.koenig@js-soft.com>

--- apps/enmeshed/lib/core/widgets/contact_item.dart
+++ apps/enmeshed/lib/core/widgets/contact_item.dart
@@ -1,8 +1,10 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
+import 'package:enmeshed_ui_kit/enmeshed_ui_kit.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/utils.dart';
 import 'contact_circle_avatar.dart';
+import 'contact_status_text.dart';
 import 'highlight_text.dart';
 
 class ContactItem extends StatelessWidget {
@@ -12,7 +14,7 @@ class ContactItem extends StatelessWidget {
   final Widget? subtitle;
   final String? query;
   final int iconSize;
-  final Color? tileColor;
+  final LocalRequestDVO? openContactRequest;
 
   const ContactItem({
     required this.contact,
@@ -21,31 +23,43 @@ class ContactItem extends StatelessWidget {
     this.subtitle,
     this.query,
     this.iconSize = 56,
-    this.tileColor,
+    this.openContactRequest,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
+    final contactStatusWidget =
+        (ContactStatusText.canRenderStatusText(contact: contact)
+            ? ContactStatusText(contact: contact, style: Theme.of(context).textTheme.labelMedium)
+            : null);
+
     return ListTile(
-      tileColor: tileColor,
       contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      leading: ContactCircleAvatar(contact: contact, radius: iconSize / 2),
+      leading: ContactCircleAvatar(
+        contact: contact,
+        radius: iconSize / 2,
+        borderColor: _getCircularAvatarBorderColor(context: context, contact: contact, openContactRequest: openContactRequest),
+      ),
       title: HighlightText(query: query, text: contact.isUnknown ? context.l10n.contacts_unknown : contact.name),
-      subtitle:
-          subtitle ??
-          switch (contact.relationship?.status) {
-            RelationshipStatus.Pending => Text(context.l10n.contacts_pending, style: TextStyle(color: Theme.of(context).colorScheme.secondary)),
-            RelationshipStatus.Terminated => Text(context.l10n.contacts_terminated, style: TextStyle(color: Theme.of(context).colorScheme.error)),
-            RelationshipStatus.DeletionProposed => Text(
-              context.l10n.contacts_deletionProposed,
-              style: TextStyle(color: Theme.of(context).colorScheme.error),
-            ),
-            null => Text(context.l10n.contacts_notYetRequested, style: TextStyle(color: Theme.of(context).colorScheme.secondary)),
-            _ => null,
-          },
+      subtitle: subtitle ?? contactStatusWidget,
       trailing: trailing,
       onTap: onTap,
     );
+  }
+
+  Color? _getCircularAvatarBorderColor({required BuildContext context, required IdentityDVO contact, LocalRequestDVO? openContactRequest}) {
+    if (openContactRequest != null ||
+        contact.relationship?.peerDeletionStatus == PeerDeletionStatus.Deleted ||
+        contact.relationship?.status == RelationshipStatus.Terminated ||
+        contact.relationship?.status == RelationshipStatus.DeletionProposed) {
+      return Theme.of(context).colorScheme.error;
+    }
+
+    if (contact.relationship?.peerDeletionStatus == PeerDeletionStatus.ToBeDeleted) return context.customColors.warning;
+
+    if (contact.relationship?.status == RelationshipStatus.Pending) return Theme.of(context).colorScheme.secondary;
+
+    return null;
   }
 }
