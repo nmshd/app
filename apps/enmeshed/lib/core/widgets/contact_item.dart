772ef58238a4cbcbf6837796db854b26f02a48bd
Ali Ince <ince.ali@msn.com>
2025-03-17 16:23:37 +0100

    fix: merging

--- apps/enmeshed/lib/core/widgets/contact_item.dart
+++ apps/enmeshed/lib/core/widgets/contact_item.dart
@@ -49,6 +49,10 @@ class ContactItem extends StatelessWidget {
   }
 
   Color _getCircularAvatarBorderColor({required BuildContext context, required IdentityDVO contact, LocalRequestDVO? openContactRequest}) {
+    if (contact.relationship?.status == RelationshipStatus.Pending || (openContactRequest?.peer.hasRelationship ?? false)) {
+      return Theme.of(context).colorScheme.secondary;
+    }
+
     if (openContactRequest != null ||
         contact.relationship?.peerDeletionStatus == PeerDeletionStatus.Deleted ||
         contact.relationship?.status == RelationshipStatus.Terminated ||
@@ -57,8 +61,6 @@ class ContactItem extends StatelessWidget {
     }
 
     if (contact.relationship?.peerDeletionStatus == PeerDeletionStatus.ToBeDeleted) return context.customColors.warning;
-
-    if (contact.relationship?.status == RelationshipStatus.Pending) return Theme.of(context).colorScheme.secondary;
 
     return Colors.transparent;
   }
