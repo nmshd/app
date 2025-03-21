2da3ae0d86c510a5a095931ca90a56fd0073b755
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-03-21 14:39:32 +0100

    feat: generate for AuthenticationRequestItem

--- packages/enmeshed_types/lib/src/contents/request_items/authentication_request_item.dart
+++ packages/enmeshed_types/lib/src/contents/request_items/authentication_request_item.dart
@@ -1,20 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
 import 'request_item_derivation.dart';
 
+part 'authentication_request_item.g.dart';
+
+@JsonSerializable(includeIfNull: false)
 class AuthenticationRequestItem extends RequestItemDerivation {
   const AuthenticationRequestItem({super.title, super.description, super.metadata, required super.mustBeAccepted, super.requireManualDecision});
 
-  factory AuthenticationRequestItem.fromJson(Map json) {
-    return AuthenticationRequestItem(
-      title: json['title'],
-      description: json['description'],
-      metadata: json['metadata'] != null ? Map<String, dynamic>.from(json['metadata']) : null,
-      mustBeAccepted: json['mustBeAccepted'],
-      requireManualDecision: json['requireManualDecision'],
-    );
-  }
-
+  factory AuthenticationRequestItem.fromJson(Map json) => _$AuthenticationRequestItemFromJson(Map<String, dynamic>.from(json));
   @override
-  Map<String, dynamic> toJson() => {...super.toJson(), '@type': 'AuthenticationRequestItem'};
+  Map<String, dynamic> toJson() {
+    final json = super.toJson();
+    json.addAll(_$AuthenticationRequestItemToJson(this));
+    return json;
+  }
 
   @override
   List<Object?> get props => [super.props];
