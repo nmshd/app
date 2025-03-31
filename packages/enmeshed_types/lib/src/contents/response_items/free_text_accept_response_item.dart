a90b67868d0497d390438c3333c73da296df1532
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-03-31 11:07:11 +0200

    feat: generate FreeTextRequestItem

--- packages/enmeshed_types/lib/src/contents/response_items/free_text_accept_response_item.dart
+++ packages/enmeshed_types/lib/src/contents/response_items/free_text_accept_response_item.dart
@@ -2,8 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 import 'accept_response_item.dart';
 
-// TODO: why doesn't this work?
-// part 'free_text_accept_response_item.g.dart';
+part 'free_text_accept_response_item.g.dart';
 
 @JsonSerializable(includeIfNull: false)
 class FreeTextAcceptResponseItem extends AcceptResponseItem {
---
+++ packages/enmeshed_types/lib/src/contents/response_items/free_text_accept_response_item.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'free_text_accept_response_item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FreeTextAcceptResponseItem _$FreeTextAcceptResponseItemFromJson(Map<String, dynamic> json) =>
+    FreeTextAcceptResponseItem(freeText: json['freeText'] as String);
+
+Map<String, dynamic> _$FreeTextAcceptResponseItemToJson(FreeTextAcceptResponseItem instance) => <String, dynamic>{'freeText': instance.freeText};
