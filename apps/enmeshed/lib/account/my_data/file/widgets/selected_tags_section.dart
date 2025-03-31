284fdcd167b269aca81e3b7c831f97a09f7c5ed8
Ali Ince <ince.ali@msn.com>
2025-03-31 09:25:17 +0200

    feat: display selected tags

--- apps/enmeshed/lib/account/my_data/file/widgets/selected_tags_section.dart
+++ apps/enmeshed/lib/account/my_data/file/widgets/selected_tags_section.dart
@@ -6,31 +6,47 @@ import '../utils/get_tag_label.dart';
 class SelectedTagsSection extends StatelessWidget {
   final AttributeTagCollectionDTO tagCollection;
   final List<String> selectedTagsList;
+  final void Function({required String tagPath, required bool selected})? onTagDeleted;
 
-  const SelectedTagsSection({required this.tagCollection, required this.selectedTagsList, super.key});
+  const SelectedTagsSection({required this.tagCollection, required this.selectedTagsList, this.onTagDeleted, super.key});
 
   @override
   Widget build(BuildContext context) {
-    final availableTags = tagCollection.tagsForAttributeValueTypes['IdentityFileReference'] ?? {};
-    if (availableTags.isEmpty) return const SizedBox.shrink();
-
     return Wrap(
       spacing: 10,
       children:
           selectedTagsList.map((tagPath) {
-            final tag = availableTags[tagPath];
+            final tag = _getTagByPath(context, tagCollection, tagPath);
+
             if (tag == null) return const SizedBox.shrink();
 
             return Chip(
               label: Text(
                 getTagLabel(context, tagCollection, tag),
-                style: Theme.of(context).textTheme.labelSmall?.copyWith(color: Theme.of(context).colorScheme.onSecondaryContainer),
+                style:
+                    onTagDeleted == null
+                        ? Theme.of(context).textTheme.labelSmall?.copyWith(color: Theme.of(context).colorScheme.onSecondaryContainer)
+                        : Theme.of(context).textTheme.labelLarge?.copyWith(color: Theme.of(context).colorScheme.onSecondaryContainer),
               ),
+              deleteIcon: onTagDeleted == null ? null : const Icon(Icons.close),
+              onDeleted: onTagDeleted == null ? null : () => onTagDeleted!(tagPath: tagPath, selected: false),
               side: BorderSide.none,
               backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
               padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
             );
           }).toList(),
     );
+  }
+
+  AttributeTagDTO? _getTagByPath(BuildContext context, AttributeTagCollectionDTO tagCollection, String tagPath) {
+    if (tagPath.isEmpty) return null;
+
+    final availableTags = tagCollection.tagsForAttributeValueTypes['IdentityFileReference'] ?? {};
+    if (!tagPath.contains('+%+')) return availableTags[tagPath];
+
+    return tagPath.split('+%+').fold<AttributeTagDTO?>(null, (currentTag, part) {
+      if (currentTag == null) return availableTags[part];
+      return currentTag.children?[part];
+    });
   }
 }
