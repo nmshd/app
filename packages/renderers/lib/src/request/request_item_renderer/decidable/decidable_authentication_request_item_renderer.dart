03371a295fae6b4ce22b87c63f63d572e034e8f7
Nicole Ebken <nicole.ebken@js-soft.com>
2025-04-04 16:53:48 +0200

    chore: add toggle

--- packages/renderers/lib/src/request/request_item_renderer/decidable/decidable_authentication_request_item_renderer.dart
+++ packages/renderers/lib/src/request/request_item_renderer/decidable/decidable_authentication_request_item_renderer.dart
@@ -1,7 +1,6 @@
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
 
-import '../../../custom_list_tile.dart';
 import '../../request_item_index.dart';
 import '../../request_renderer_controller.dart';
 import 'checkbox_enabled_extension.dart';
@@ -20,6 +19,7 @@ class DecidableAuthenticationRequestItemRenderer extends StatefulWidget {
 
 class _DecidableAuthenticationRequestItemRendererState extends State<DecidableAuthenticationRequestItemRenderer> {
   late bool isChecked;
+  bool _value = false;
 
   @override
   void initState() {
@@ -36,8 +36,28 @@ class _DecidableAuthenticationRequestItemRendererState extends State<DecidableAu
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(value: isChecked, onChanged: widget.item.checkboxEnabled ? onUpdateCheckbox : null),
-        Expanded(child: CustomListTile(title: 'i18n://dvo.requestItem.DecidableAuthenticationRequestItem.name', thirdLine: widget.item.name)),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(widget.item.name, style: Theme.of(context).textTheme.titleMedium),
+                SizedBox(height: 4),
+                if (widget.item.description != null) Text(widget.item.description!, style: Theme.of(context).textTheme.bodySmall),
+                Container(
+                  width: double.infinity,
+                  color: Theme.of(context).colorScheme.surfaceContainerHigh,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: _CustomToggle(value: _value, onChanged: (newValue) => setState(() => _value = newValue), text: 'Annehmen'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
@@ -50,5 +70,49 @@ class _DecidableAuthenticationRequestItemRendererState extends State<DecidableAu
     });
 
     handleCheckboxChange(isChecked: isChecked, controller: widget.controller, itemIndex: widget.itemIndex);
+  }
+}
+
+class _CustomToggle extends StatefulWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final String text;
+
+  const _CustomToggle({required this.value, required this.onChanged, required this.text});
+
+  @override
+  State<_CustomToggle> createState() => _CustomToggleState();
+}
+
+class _CustomToggleState extends State<_CustomToggle> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => widget.onChanged(!widget.value),
+      child: Container(
+        height: 55,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: Theme.of(context).colorScheme.surface, border: Border()),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            AnimatedAlign(
+              duration: const Duration(milliseconds: 400),
+              curve: Curves.easeInOutCubic,
+              alignment: widget.value ? Alignment.centerRight : Alignment.centerLeft,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 400),
+                curve: Curves.easeInOutCubic,
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(color: widget.value ? Colors.green : Colors.blue, shape: BoxShape.circle),
+                child: Icon(Icons.arrow_back_ios_new_outlined),
+              ),
+            ),
+            Text(widget.text, style: Theme.of(context).textTheme.headlineSmall, textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
   }
 }
