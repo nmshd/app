48f40d56f6a2e6ce502a5e1bdcd31d6f6409eefc
Ali Ince <ince.ali@msn.com>
2025-04-08 19:42:59 +0200

    feat: update DecidableConsentRequestItemRenderer - add showFullConsent button - add alert container - add function to check if consent text is overflowing

--- packages/renderers/lib/src/request/request_item_renderer/decidable/decidable_consent_request_item_renderer.dart
+++ packages/renderers/lib/src/request/request_item_renderer/decidable/decidable_consent_request_item_renderer.dart
@@ -44,66 +44,89 @@ class _DecidableConsentRequestItemRendererState extends State<DecidableConsentRe
         children: [
           Text(widget.item.name, style: Theme.of(context).textTheme.titleMedium),
           if (widget.item.description != null) Text(widget.item.description!, style: Theme.of(context).textTheme.bodySmall),
-          if ((widget.item.requireManualDecision != null && widget.item.requireManualDecision!) || true && widget.item.consent.isNotEmpty) ...[
-            Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: Material(
-                clipBehavior: Clip.hardEdge,
-                borderRadius: BorderRadius.circular(4),
-                color: Theme.of(context).colorScheme.surfaceContainerHighest,
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          if (widget.item.requireManualDecision == true) ...[
-                            Switch(
-                              thumbIcon: WidgetStateProperty.resolveWith<Icon?>(
-                                (Set<WidgetState> states) =>
-                                    states.contains(WidgetState.selected) ? const Icon(Icons.check) : const Icon(Icons.close),
-                              ),
-                              activeColor: context.customColors.onSuccess,
-                              value: _isChecked,
-                              activeTrackColor: context.customColors.success,
-                              onChanged: widget.item.checkboxEnabled ? onUpdateCheckbox : null,
-                            ),
-                            Gaps.w8,
-                          ],
-                          Expanded(child: Text(widget.item.consent)),
-                        ],
-                      ),
-                      if (widget.item.link != null)
-                        Padding(
-                          padding: EdgeInsets.only(
-                            top: 8,
-                            left: widget.item.requireManualDecision == true && widget.item.consent.isNotEmpty ? 68 : 0,
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Material(
+              clipBehavior: Clip.hardEdge,
+              borderRadius: BorderRadius.circular(4),
+              color: Theme.of(context).colorScheme.surfaceContainerHighest,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Switch(
+                          thumbIcon: WidgetStateProperty.resolveWith<Icon?>(
+                            (Set<WidgetState> states) => states.contains(WidgetState.selected) ? const Icon(Icons.check) : const Icon(Icons.close),
                           ),
-                          child: _LinkButton(link: widget.item.link!, linkDisplayText: widget.item.linkDisplayText),
+                          activeColor: context.customColors.onSuccess,
+                          value: _isChecked,
+                          activeTrackColor: _isSwitchDisabled ? context.customColors.success.withAlpha(84) : context.customColors.success,
+                          onChanged: _isSwitchDisabled ? null : onUpdateCheckbox,
                         ),
-                    ],
-                  ),
+                        Gaps.w8,
+                        Expanded(child: Text(widget.item.consent, maxLines: 4, overflow: TextOverflow.ellipsis)),
+                      ],
+                    ),
+                    if (_isTextOverflowing()) Padding(padding: EdgeInsets.only(top: 8, left: 68), child: _ShowFullConsentButton()),
+                    if (widget.item.link != null)
+                      Padding(
+                        padding: EdgeInsets.only(top: 8, left: 68),
+                        child: _LinkButton(link: widget.item.link!, linkDisplayText: widget.item.linkDisplayText),
+                      ),
+                  ],
                 ),
               ),
             ),
-          ] else if (widget.item.link != null) ...[
-            Gaps.h8,
-            _LinkButton(link: widget.item.link!, linkDisplayText: widget.item.linkDisplayText),
-          ],
+          ),
+          if (widget.item.mustBeAccepted && widget.item.requireManualDecision == true && !_isChecked) _AlertContainer(),
         ],
       ),
     );
   }
 
+  bool get _isSwitchDisabled => widget.item.mustBeAccepted && widget.item.requireManualDecision == false;
+
   void onUpdateCheckbox(bool? value) {
     if (value == null) return;
-
     setState(() {
       _isChecked = value;
     });
-
     handleCheckboxChange(isChecked: _isChecked, controller: widget.controller, itemIndex: widget.itemIndex);
+  }
+
+  bool _isTextOverflowing() {
+    final textPainter = TextPainter(
+      text: TextSpan(text: widget.item.consent, style: Theme.of(context).textTheme.bodyMedium),
+      maxLines: 4,
+      textDirection: TextDirection.ltr,
+    )..layout(maxWidth: MediaQuery.sizeOf(context).width - 116);
+    return textPainter.didExceedMaxLines;
+  }
+}
+
+class _ShowFullConsentButton extends StatelessWidget {
+  const _ShowFullConsentButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      // TODO: open full screen dialog
+      onTap: () {},
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TranslatedText(
+            'i18n://requestRenderer.showFullConsent',
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.primary),
+          ),
+          Gaps.w8,
+          Icon(Icons.article_outlined, color: Theme.of(context).colorScheme.primary, size: 14),
+        ],
+      ),
+    );
   }
 }
 
@@ -111,8 +134,7 @@ class _LinkButton extends StatelessWidget {
   final String link;
   final String? linkDisplayText;
 
-  const _LinkButton({required this.link, required this.linkDisplayText});
-
+  const _LinkButton({required this.link, this.linkDisplayText});
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -129,6 +151,31 @@ class _LinkButton extends StatelessWidget {
           ),
           Gaps.w8,
           Icon(Icons.arrow_outward, color: Theme.of(context).colorScheme.primary, size: 12),
+        ],
+      ),
+    );
+  }
+}
+
+class _AlertContainer extends StatelessWidget {
+  const _AlertContainer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Theme.of(context).colorScheme.error, borderRadius: BorderRadius.circular(4)),
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.all(4),
+      child: Row(
+        children: [
+          Icon(Icons.error, color: Theme.of(context).colorScheme.onError),
+          Gaps.w4,
+          Expanded(
+            child: TranslatedText(
+              'i18n://requestRenderer.consentRequired',
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(color: Theme.of(context).colorScheme.onError),
+            ),
+          ),
         ],
       ),
     );
