e7b65edb230adaadb43d056920aa428e259d036b
Julian König <julian.koenig@js-soft.com>
2025-04-08 15:28:39 +0200

    feat: translations

--- apps/enmeshed/lib/account/app_drawer/drawer_theme_page.dart
+++ apps/enmeshed/lib/account/app_drawer/drawer_theme_page.dart
@@ -12,6 +12,7 @@ class DrawerThemePage extends StatelessWidget with WatchItMixin {
 
   @override
   Widget build(BuildContext context) {
+    const borderRadius = BorderRadius.all(Radius.circular(32));
     final themeSetting = watchValue((ThemeModeModel x) => x.notifier);
 
     return Column(
@@ -21,7 +22,7 @@ class DrawerThemePage extends StatelessWidget with WatchItMixin {
           children: [
             IconButton(icon: const Icon(Icons.arrow_back), onPressed: goBack),
             Text(
-              context.l10n.drawer_notifications,
+              context.l10n.drawer_theme,
               style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onPrimaryContainer),
             ),
             const Spacer(),
@@ -34,45 +35,36 @@ class DrawerThemePage extends StatelessWidget with WatchItMixin {
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      // TODO(jkoenig134): translate
-                      'Hier können Sie das Erscheinungsbild der App einstellen. Wenn Sie die Einstellung System wählen wird die aktuelle Standardeinstellung Ihres Geräts übernommen.',
-                    ),
-                  ),
+                  Padding(padding: const EdgeInsets.all(16), child: Text(context.l10n.drawer_theme_description)),
                   RadioListTile(
-                    title: const Text('System'),
+                    shape: const RoundedRectangleBorder(borderRadius: borderRadius),
+                    title: Text(context.l10n.drawer_theme_modes_system),
                     groupValue: themeSetting.themeMode,
                     value: ThemeMode.system,
                     onChanged: (v) => GetIt.I.get<ThemeModeModel>().setThemeMode(ThemeMode.system),
                   ),
                   RadioListTile(
-                    title: const Text('Light'),
+                    shape: const RoundedRectangleBorder(borderRadius: borderRadius),
+                    title: Text(context.l10n.drawer_theme_modes_light),
                     groupValue: themeSetting.themeMode,
                     value: ThemeMode.light,
                     onChanged: (v) => GetIt.I.get<ThemeModeModel>().setThemeMode(ThemeMode.light),
                   ),
                   RadioListTile(
-                    title: const Text('Dark'),
+                    shape: const RoundedRectangleBorder(borderRadius: borderRadius),
+                    title: Text(context.l10n.drawer_theme_modes_dark),
                     groupValue: themeSetting.themeMode,
                     value: ThemeMode.dark,
                     onChanged: (v) => GetIt.I.get<ThemeModeModel>().setThemeMode(ThemeMode.dark),
                   ),
                   Gaps.h8,
                   SwitchListTile(
+                    shape: const RoundedRectangleBorder(borderRadius: borderRadius),
                     title: const Text('AMOLED'),
-                    secondary: const Icon(Icons.nightlight_round),
                     value: themeSetting.amoled,
                     onChanged: (v) => GetIt.I.get<ThemeModeModel>().setAmoled(amoled: v),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      // TODO(jkoenig134): translate
-                      'Der AMOLED-Modus hilft Energie zu sparen indem Pixel, die auf schwarz gesetzt sind, vollständig abgeschaltet werden. Diese Einstellung hat nur Einfluss auf das dunkle Erscheinungsbild.',
-                    ),
-                  ),
+                  Padding(padding: const EdgeInsets.all(16), child: Text(context.l10n.drawer_theme_amoled)),
                 ],
               ),
             ),
