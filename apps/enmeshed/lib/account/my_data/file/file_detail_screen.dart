b651b06f9d67d666675532fa6a642bc5378cbc6b
Julian König <julian.koenig@js-soft.com>
2025-04-09 13:30:40 +0200

    refactor: spacings & colors

--- apps/enmeshed/lib/account/my_data/file/file_detail_screen.dart
+++ apps/enmeshed/lib/account/my_data/file/file_detail_screen.dart
@@ -10,7 +10,7 @@ import '/core/core.dart';
 class FileDetailScreen extends StatefulWidget {
   final String accountId;
   final String fileId;
-  final FileDVO preLoadedFile;
+  final FileDVO? preLoadedFile;
   final LocalAttributeDVO? fileReferenceAttribute;
 
   const FileDetailScreen({required this.accountId, required this.fileId, required this.preLoadedFile, this.fileReferenceAttribute, super.key});
@@ -42,78 +42,101 @@ class _FileDetailScreenState extends State<FileDetailScreen> {
       appBar: AppBar(title: Text(_fileDVO!.title, style: Theme.of(context).textTheme.titleLarge)),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 8, left: 24, right: 24),
+          padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
           child:
               _fileDVO == null
                   ? const Center(child: CircularProgressIndicator())
                   : Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 16,
                     children: [
                       Center(
                         child: Column(
                           children: [
-                            FileIcon(filename: _fileDVO!.filename, color: Theme.of(context).colorScheme.primaryContainer, size: 40),
+                            FileIcon(filename: _fileDVO!.filename, color: Theme.of(context).colorScheme.primary, size: 40),
                             Gaps.h8,
                             Text(_fileDVO!.filename, style: Theme.of(context).textTheme.labelLarge),
                             Text('${bytesText(context: context, bytes: _fileDVO!.filesize)} - ${getFileExtension(_fileDVO!.filename)}'),
                           ],
                         ),
                       ),
-                      Gaps.h24,
                       if (_tags != null)
-                        Chip(
-                          label: Text(_tags!.join(', '), style: TextStyle(color: Theme.of(context).colorScheme.onSecondaryContainer)),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: const BorderRadius.all(Radius.circular(8)),
-                            side: BorderSide(color: Theme.of(context).colorScheme.secondaryContainer),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Wrap(
+                            spacing: 12,
+                            // alignment: WrapAlignment.spaceBetween,
+                            children:
+                                _tags!
+                                    .map(
+                                      (e) => Chip(
+                                        label: Text(e, style: TextStyle(color: Theme.of(context).colorScheme.onSecondaryContainer)),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: const BorderRadius.all(Radius.circular(4)),
+                                          side: BorderSide(color: Theme.of(context).colorScheme.secondaryContainer),
+                                        ),
+                                        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+                                        padding: EdgeInsets.zero,
+                                        labelPadding: const EdgeInsets.symmetric(horizontal: 6),
+                                        visualDensity: const VisualDensity(horizontal: -2),
+                                      ),
+                                    )
+                                    .toList(),
                           ),
-                          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-                          padding: EdgeInsets.zero,
-                          labelPadding: const EdgeInsets.symmetric(horizontal: 6),
                         ),
-                      Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                      Card(
+                        margin: EdgeInsets.zero,
+                        elevation: 0,
+                        color: Theme.of(context).colorScheme.surfaceContainer,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                          child: Row(
                             children: [
-                              Text(
-                                '${context.l10n.files_owner}: ',
-                                style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${context.l10n.files_owner}: ',
+                                    style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+                                  ),
+                                  Text(
+                                    '${context.l10n.files_createdAt}: ',
+                                    style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                '${context.l10n.files_createdAt}: ',
-                                style: Theme.of(context).textTheme.labelLarge!.copyWith(color: Theme.of(context).colorScheme.onSurfaceVariant),
+                              Gaps.w24,
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(context.i18nTranslate(_fileDVO!.createdBy.name), style: Theme.of(context).textTheme.bodyMedium),
+                                  Text(
+                                    context.i18nTranslate(_formatDate(context, _fileDVO!.createdAt)),
+                                    style: Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                ],
                               ),
                             ],
                           ),
-                          Gaps.w24,
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(context.i18nTranslate(_fileDVO!.createdBy.name), style: Theme.of(context).textTheme.bodyMedium),
-                              Text(context.i18nTranslate(_formatDate(context, _fileDVO!.createdAt)), style: Theme.of(context).textTheme.bodyMedium),
-                            ],
-                          ),
-                        ],
+                        ),
                       ),
-                      Gaps.h32,
                       Row(
+                        spacing: 8,
                         children: [
-                          Gaps.w8,
                           IconButton(
                             onPressed: _isLoadingFile || DateTime.parse(_fileDVO!.expiresAt).isBefore(DateTime.now()) ? null : _downloadAndSaveFile,
                             icon:
                                 _isLoadingFile
-                                    ? const SizedBox(width: 24, height: 24, child: CircularProgressIndicator())
-                                    : const Icon(Icons.file_download, size: 24),
+                                    ? const SizedBox(width: 28, height: 28, child: CircularProgressIndicator(strokeWidth: 3))
+                                    : const Icon(Icons.file_download_outlined, size: 24),
                           ),
-                          Gaps.w8,
                           IconButton(
                             onPressed: _isOpeningFile || DateTime.parse(_fileDVO!.expiresAt).isBefore(DateTime.now()) ? null : _openFile,
                             icon:
                                 _isOpeningFile
-                                    ? const SizedBox(width: 24, height: 24, child: CircularProgressIndicator())
-                                    : const Icon(Icons.open_with, size: 24),
+                                    ? const SizedBox(width: 28, height: 28, child: CircularProgressIndicator(strokeWidth: 3))
+                                    : const Icon(Icons.file_open_outlined, size: 24),
                           ),
                         ],
                       ),
@@ -132,7 +155,7 @@ class _FileDetailScreenState extends State<FileDetailScreen> {
 
   Future<void> _load() async {
     final session = GetIt.I.get<EnmeshedRuntime>().getSession(widget.accountId);
-    final response = await session.transportServices.files.getFile(fileId: widget.preLoadedFile.id);
+    final response = await session.transportServices.files.getFile(fileId: widget.fileId);
     final expanded = await session.expander.expandFileDTO(response.value);
 
     setState(() => _fileDVO = expanded);
