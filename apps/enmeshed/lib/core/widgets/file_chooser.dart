5478a18863edd4dbac8ec887166da7aadca5181f
Nicole Ebken <nicole.ebken@js-soft.com>
2025-04-15 11:34:46 +0200

    fix: use translated text

--- apps/enmeshed/lib/core/widgets/file_chooser.dart
+++ apps/enmeshed/lib/core/widgets/file_chooser.dart
@@ -3,6 +3,7 @@ import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:i18n_translated_text/i18n_translated_text.dart';
 
 import '../utils/extensions.dart';
 import '../widgets/widgets.dart';
@@ -106,7 +107,7 @@ class _FileChooserState extends State<_FileChooser> {
                             final file = _existingFiles![index];
 
                             return ListTile(
-                              title: Text(file.title),
+                              title: TranslatedText(file.title),
                               subtitle: Text(file.filename),
                               onTap: () {
                                 if (widget.selectedFiles == null) return context.pop(file);
