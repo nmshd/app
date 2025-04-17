a667926fdbe2b1bce5c844c089cbb6802b1752a4
Britta Stallknecht <146106656+britsta@users.noreply.github.com>
2025-04-17 15:45:16 +0200

    Add Support for FormFieldRequestItem (#531)
    * feat: add translations
    
    * chore: update Runtime to version 6.37.0
    
    * feat: add FormFieldSettings
    
    * feat: add FormFieldRequestItem
    
    * chore: auto-generated files
    
    * fix: copy-paste error
    
    * feat: add FormFieldAcceptResponseItem
    
    * feat: add AcceptFormFieldRequestItemParameters
    
    * feat: add FormFieldAcceptResponseType
    
    * feat: use simplified FormFieldAcceptResponseItem
    
    * feat: incorporate review comments
    
    * fix: re-enable analyzer for generated files
    
    * refactor: start splitting to files
    
    * refactor: move folder
    
    * refactor: move settings to their own files
    
    * feat: incorporate review comment
    
    * feat: incorporate another review comment
    
    * refactor: change order of settings
    
    ---------
    
    Co-authored-by: Julian König <julian.koenig@js-soft.com>
    Co-authored-by: Julian König <33655937+jkoenig134@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ packages/enmeshed_types/lib/src/contents/response_items/form_field_accept_response_item.dart
@@ -0,0 +1,70 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'accept_response_item.dart';
+
+part 'form_field_accept_response_item.g.dart';
+
+@JsonSerializable(includeIfNull: false)
+class FormFieldAcceptResponseItem extends AcceptResponseItem {
+  final FormFieldAcceptResponseType response;
+
+  const FormFieldAcceptResponseItem({required this.response}) : super(atType: 'FormFieldAcceptResponseItem');
+
+  factory FormFieldAcceptResponseItem.fromJson(Map json) => _$FormFieldAcceptResponseItemFromJson(Map<String, dynamic>.from(json));
+
+  @override
+  Map<String, dynamic> toJson() => _$FormFieldAcceptResponseItemToJson(this);
+
+  @override
+  List<Object?> get props => [...super.props, response];
+}
+
+sealed class FormFieldAcceptResponseType {
+  const FormFieldAcceptResponseType();
+
+  factory FormFieldAcceptResponseType.fromJson(dynamic json) => switch (json) {
+    String() => FormFieldStringResponse(json),
+    num() => FormFieldNumResponse(json),
+    bool() => FormFieldBoolResponse(json),
+    List<String>() => FormFieldStringListResponse(json),
+    _ => throw Exception('Invalid type for FormFieldAcceptResponseType: ${json.runtimeType}'),
+  };
+
+  dynamic toJson();
+}
+
+class FormFieldStringResponse implements FormFieldAcceptResponseType {
+  final String value;
+
+  const FormFieldStringResponse(this.value);
+
+  @override
+  String toJson() => value;
+}
+
+class FormFieldNumResponse implements FormFieldAcceptResponseType {
+  final num value;
+
+  const FormFieldNumResponse(this.value);
+
+  @override
+  num toJson() => value;
+}
+
+class FormFieldBoolResponse implements FormFieldAcceptResponseType {
+  final bool value;
+
+  const FormFieldBoolResponse(this.value);
+
+  @override
+  bool toJson() => value;
+}
+
+class FormFieldStringListResponse implements FormFieldAcceptResponseType {
+  final List<String> value;
+
+  const FormFieldStringListResponse(this.value);
+
+  @override
+  List<String> toJson() => value;
+}
