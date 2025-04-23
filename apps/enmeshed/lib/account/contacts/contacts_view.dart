1aa02fa37e05016710f1672a0321d85b1eab88fd
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-04-23 15:26:21 +0200

    Remove outdated features (#539)
    * chore: remove button
    
    * chore: remove modal
    
    * chore: remove translations
    
    * chore: remove more outdated features

--- apps/enmeshed/lib/account/contacts/contacts_view.dart
+++ apps/enmeshed/lib/account/contacts/contacts_view.dart
@@ -231,13 +231,6 @@ class _ContactsViewState extends State<ContactsView> {
         // this is only a workaround and only practical for the PoC
         referencesResult.value.where((e) => !relationships.any((r) => (r.originalName ?? r.name) == e.title)),
       );
-
-      if (mounted && context.isFeatureEnabled('SHOW_ADDITIONAL_PUBLIC_RELATIONSHIP_TEMPLATE_REFERENCES')) {
-        templateReferences.addAll([
-          const PublicRelationshipTemplateReferenceDTO(title: 'Uni Magdeburg', description: 'Uni Magdeburg', truncatedReference: 'none'),
-          const PublicRelationshipTemplateReferenceDTO(title: 'Lernpfadfinder', description: 'Uni Magdeburg', truncatedReference: 'none'),
-        ]);
-      }
     }
 
     if (mounted) {
--- apps/enmeshed/lib/account/contacts/modals/modals.dart
+++ apps/enmeshed/lib/account/contacts/modals/modals.dart
@@ -1,3 +1,2 @@
 export 'rename_contact.dart';
-export 'request_certificate.dart';
 export 'select_contacts_filters.dart';
