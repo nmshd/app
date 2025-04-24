71814ca4cb78437bdf476b82da5fc138b1e2320f
Julian König <julian.koenig@js-soft.com>
2025-04-24 17:41:43 +0200

    refactor: simplify & prettify literally everything

--- packages/renderers/lib/src/request/request_item_renderer/authentication_request_item_renderer.dart
+++ packages/renderers/lib/src/request/request_item_renderer/authentication_request_item_renderer.dart
@@ -2,11 +2,11 @@ import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:enmeshed_ui_kit/enmeshed_ui_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:i18n_translated_text/i18n_translated_text.dart';
-import 'package:renderers/src/request/request_item_renderer/decidable/checkbox_enabled_extension.dart';
 
 import '../request_item_index.dart';
 import '../request_renderer_controller.dart';
+import 'decidable/checkbox_enabled_extension.dart';
+import 'decidable/widgets/validation_error_box.dart';
 
 class AuthenticationRequestItemRenderer extends StatefulWidget {
   final AuthenticationRequestItemDVO item;
@@ -38,76 +38,25 @@ class _AuthenticationRequestItemRendererState extends State<AuthenticationReques
 
   @override
   Widget build(BuildContext context) {
-    final title = widget.item.mustBeAccepted && widget.item.response == null ? '${widget.item.name}*' : widget.item.name;
-
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(title, style: Theme.of(context).textTheme.titleMedium!.copyWith(fontSize: 16)),
-                    Gaps.h4,
-                    if (widget.item.description != null) Text(widget.item.description!, style: Theme.of(context).textTheme.bodySmall),
-                    Container(
-                      width: double.infinity,
-                      color: Theme.of(context).colorScheme.surfaceContainerHigh,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: _CustomAnimatedToggleSwitch(
-                          value: _isChecked,
-                          onUpdateToggle: _onUpdateToggle,
-                          text: 'Annehmen',
-                          isDecidable: widget.item.isDecidable,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-        if (widget.item.isDecidable && widget.validationResult != null && !widget.validationResult!.isSuccess)
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
-                  child: Container(
-                    color: Theme.of(context).colorScheme.error,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 6),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 4),
-                            child: Icon(Icons.error, color: Theme.of(context).colorScheme.onError),
-                          ),
-                          TranslatedText(
-                            'i18n://requestRenderer.errors.${widget.validationResult!.code!}',
-                            style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Theme.of(context).colorScheme.onError),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text.rich(
+            TextSpan(children: [TextSpan(text: widget.item.name), if (widget.item.isDecidable && widget.item.mustBeAccepted) TextSpan(text: '*')]),
+            style: Theme.of(context).textTheme.titleMedium,
           ),
-      ],
+          if (widget.item.description != null) Text(widget.item.description!, style: Theme.of(context).textTheme.bodySmall),
+          Gaps.h8,
+          _AuthenticationBox(value: _isChecked, onUpdateToggle: _onUpdateToggle, text: 'Annehmen', active: widget.item.isDecidable),
+          if (!(widget.validationResult?.isSuccess ?? true)) ...[Gaps.h8, ValidationErrorBox(validationResult: widget.validationResult!)],
+        ],
+      ),
     );
   }
 
-  void _onUpdateToggle(bool? value) {
-    if (value == null) return;
-
+  void _onUpdateToggle(bool value) {
     setState(() => _isChecked = value);
 
     if (_isChecked) {
@@ -118,66 +67,56 @@ class _AuthenticationRequestItemRendererState extends State<AuthenticationReques
   }
 }
 
-class _CustomAnimatedToggleSwitch extends StatefulWidget {
+class _AuthenticationBox extends StatelessWidget {
   final bool value;
   final ValueChanged<bool> onUpdateToggle;
   final String text;
-  final bool isDecidable;
+  final bool active;
 
-  const _CustomAnimatedToggleSwitch({required this.value, required this.onUpdateToggle, required this.text, required this.isDecidable});
+  const _AuthenticationBox({required this.value, required this.onUpdateToggle, required this.text, required this.active});
 
-  @override
-  State<_CustomAnimatedToggleSwitch> createState() => _CustomAnimatedToggleSwitchState();
-}
-
-class _CustomAnimatedToggleSwitchState extends State<_CustomAnimatedToggleSwitch> {
   @override
   Widget build(BuildContext context) {
-    final circleColor = widget.value ? context.customColors.success : Theme.of(context).colorScheme.primary;
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), color: Theme.of(context).colorScheme.surfaceContainerHigh),
+      padding: const EdgeInsets.all(16),
       child: Container(
-        height: 64,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: Theme.of(context).colorScheme.surface, border: Border()),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30), color: Theme.of(context).colorScheme.surface),
         child: Stack(
           alignment: Alignment.center,
           children: [
             Text(
-              widget.text,
-              style:
-                  widget.isDecidable
-                      ? Theme.of(context).textTheme.headlineSmall
-                      : Theme.of(context).textTheme.headlineSmall!.copyWith(color: Theme.of(context).colorScheme.onSurface.withAlpha(96)),
+              text,
+              style: Theme.of(
+                context,
+              ).textTheme.headlineSmall!.copyWith(color: Theme.of(context).colorScheme.onSurface.withValues(alpha: active ? null : 0.16)),
               textAlign: TextAlign.center,
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: AnimatedToggleSwitch<bool>.dual(
-                    current: widget.value,
-                    first: false,
-                    second: true,
-                    spacing: 0,
-                    height: 55,
-                    indicatorSize: const Size(50, 50),
-                    indicatorTransition: ForegroundIndicatorTransition.fading(),
-                    style: ToggleStyle(
-                      borderColor: Colors.transparent,
-                      backgroundColor: Colors.transparent,
-                      indicatorColor: widget.isDecidable ? circleColor : circleColor.withAlpha(41),
-                      indicatorBorderRadius: BorderRadius.circular(50),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    onChanged: widget.isDecidable ? (value) => widget.onUpdateToggle(!widget.value) : null,
-                    iconBuilder:
-                        (value) =>
-                            value
-                                ? Icon(Icons.check, color: context.customColors.onSuccess)
-                                : Icon(Icons.arrow_forward_ios, color: Theme.of(context).colorScheme.onPrimary),
+            SizedBox(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(4),
+                child: AnimatedToggleSwitch<bool>.dual(
+                  active: active,
+                  current: value,
+                  first: false,
+                  second: true,
+                  indicatorSize: const Size.square(50),
+                  indicatorTransition: ForegroundIndicatorTransition.fading(),
+                  style: ToggleStyle(
+                    borderColor: Colors.transparent,
+                    backgroundColor: Colors.transparent,
+                    indicatorColor: value ? context.customColors.success : Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(50),
                   ),
+                  onChanged: active ? (value) => onUpdateToggle(!value) : null,
+                  iconBuilder:
+                      (value) =>
+                          value
+                              ? Icon(Icons.check, color: context.customColors.onSuccess)
+                              : Icon(Icons.arrow_forward_ios, color: Theme.of(context).colorScheme.onPrimary),
                 ),
-              ],
+              ),
             ),
           ],
         ),
