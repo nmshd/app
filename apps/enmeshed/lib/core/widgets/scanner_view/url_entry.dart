0a9ebab2213ce07f97ce02a1fe934edb52168a25
Julian König <julian.koenig@js-soft.com>
2025-05-14 13:06:20 +0200

    chore: add comment for regex

--- apps/enmeshed/lib/core/widgets/scanner_view/url_entry.dart
+++ apps/enmeshed/lib/core/widgets/scanner_view/url_entry.dart
@@ -95,6 +95,7 @@ class _UrlEntryState extends State<UrlEntry> {
   bool get isUrlValid => validateUrl(_controller.text) == null;
 
   String? validateUrl(String? value) {
+    // https://regex101.com/r/dnkzPG/1
     final urlRegExp = RegExp(r'^https?:\/\/.*\/r\/[a-zA-Z0-9]+#[a-zA-Z0-9]+$');
 
     if (value != null && (!urlRegExp.hasMatch(value) || value.length < 15)) {
