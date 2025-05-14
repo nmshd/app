1b3139d6624aec2da59816fd82c66feb81fc3415
Julian König <julian.koenig@js-soft.com>
2025-05-14 12:55:38 +0200

    feat: use url reference for recovery kits

--- apps/enmeshed/lib/core/modals/create_recovery_kit/create_recovery_kit.dart
+++ apps/enmeshed/lib/core/modals/create_recovery_kit/create_recovery_kit.dart
@@ -120,7 +120,7 @@ class _CreateRecoveryKitModalState extends State<_CreateRecoveryKitModal> {
       addressHexColor: '#1A80D9',
       pdfTexts: pdfTexts,
       qrSettings: (errorCorrectionLevel: QRErrorCorrectionLevel.L, qrPixelSize: null),
-    ).generate(logoBytes: logoBytes, spacerSvgImage: spacerSvgImage, backupURL: 'nmshd://tr#${token.value.reference.truncated}');
+    ).generate(logoBytes: logoBytes, spacerSvgImage: spacerSvgImage, backupURL: token.value.reference.url);
 
     return generatedPdf;
   }
