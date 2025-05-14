76d802c5b04146e8516fc0ae5b8fc1dac4a240a5
Julian König <julian.koenig@js-soft.com>
2025-05-14 13:10:00 +0200

    fix: update schemes for windows

--- apps/enmeshed/lib/splash_screen.dart
+++ apps/enmeshed/lib/splash_screen.dart
@@ -100,7 +100,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     if (mounted) await runtime.registerUIBridge(AppUIBridge(logger: logger, router: router, localizations: context.l10n));
 
-    await _registerWindowsSchemeForDebugMode('nmshd-dev');
+    await _registerWindowsSchemeForDebugMode('enmeshed-dev');
 
     final appLinks = AppLinks();
     appLinks.uriLinkStream.listen(_processUri);
