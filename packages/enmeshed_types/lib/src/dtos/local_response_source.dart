1b01237430ddb4e909efb9616a87a13693351d31
Milena Czierlinski <milena.czierlinski@js-soft.com>
2025-05-23 15:47:57 +0200

    refactor: generate local request

--- packages/enmeshed_types/lib/src/dtos/local_response_source.dart
+++ packages/enmeshed_types/lib/src/dtos/local_response_source.dart
@@ -1,18 +1,20 @@
 import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'local_response_source.g.dart';
 
 enum LocalResponseSourceType { Message, Relationship }
 
+@JsonSerializable(includeIfNull: false)
 class LocalResponseSourceDTO extends Equatable {
   final LocalResponseSourceType type;
   final String reference;
 
   const LocalResponseSourceDTO({required this.type, required this.reference});
 
-  factory LocalResponseSourceDTO.fromJson(Map json) {
-    return LocalResponseSourceDTO(type: LocalResponseSourceType.values.byName(json['type']), reference: json['reference']);
-  }
+  factory LocalResponseSourceDTO.fromJson(Map json) => _$LocalResponseSourceDTOFromJson(Map<String, dynamic>.from(json));
 
-  Map<String, dynamic> toJson() => {'type': type.name, 'reference': reference};
+  Map<String, dynamic> toJson() => _$LocalResponseSourceDTOToJson(this);
 
   @override
   List<Object?> get props => [type, reference];
