ea6bb9e700c3459bd89bf7034c7aeaa29390bed6
Julian König <julian.koenig@js-soft.com>
2025-06-25 12:06:52 +0200

    feat: set the app language

---
+++ packages/enmeshed_runtime_bridge/js/src/AppLanguageProvider.ts
@@ -0,0 +1,19 @@
+import { ApplicationError, Result } from "@js-soft/ts-utils";
+import { IAppLanguageProvider } from "@nmshd/app-runtime";
+import { LanguageISO639 } from "@nmshd/core-types";
+
+export class AppLanguageProvider implements IAppLanguageProvider {
+  public async getAppLanguage(): Promise<Result<LanguageISO639>> {
+    const language = await window.flutter_inappwebview.callHandler("getAppLanguage");
+    if (typeof language !== "string" || !Object.keys(LanguageISO639).includes(language)) {
+      return Result.fail(
+        new ApplicationError(
+          "error.appRuntimeBridge.invalidLanguageType",
+          `Invalid language '${language}' received for getAppLanguage. Expected a ISO639 language.`
+        )
+      );
+    }
+
+    return Result.ok(language as LanguageISO639);
+  }
+}
