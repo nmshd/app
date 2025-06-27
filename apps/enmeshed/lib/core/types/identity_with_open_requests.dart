fcc34b8dc54cf37108a1ffea40377c718e9f89d0
Julian König <julian.koenig@js-soft.com>
2025-06-27 14:06:11 +0200

    chore: formatting

--- apps/enmeshed/lib/core/types/identity_with_open_requests.dart
+++ apps/enmeshed/lib/core/types/identity_with_open_requests.dart
@@ -3,11 +3,9 @@ import 'package:enmeshed_types/enmeshed_types.dart';
 typedef IdentityWithOpenRequests = ({IdentityDVO contact, List<LocalRequestDVO> openRequests});
 
 extension IdentityWithOpenRequestsExtension on IdentityWithOpenRequests {
-  bool get requiresAttention {
-    return switch (this.contact.relationship?.status) {
-      null => true,
-      RelationshipStatus.Terminated || RelationshipStatus.DeletionProposed => true,
-      _ => this.openRequests.isNotEmpty,
-    };
-  }
+  bool get requiresAttention => switch (this.contact.relationship?.status) {
+    null => true,
+    RelationshipStatus.Terminated || RelationshipStatus.DeletionProposed => true,
+    _ => this.openRequests.isNotEmpty,
+  };
 }
