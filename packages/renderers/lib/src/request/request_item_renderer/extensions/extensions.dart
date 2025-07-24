b48e9a1ea00d3a178b9608a982a6d5789da53d0f
Julian König <julian.koenig@js-soft.com>
2025-07-24 16:52:27 +0200

    chore: rm old helpers

--- packages/renderers/lib/src/request/request_item_renderer/extensions/extensions.dart
+++
@@ -1,5 +0,0 @@
-import 'package:enmeshed_types/enmeshed_types.dart';
-
-extension InitiallyCheckedExtension on RequestItemDVODerivation {
-  bool get initiallyChecked => mustBeAccepted && requireManualDecision != true;
-}
