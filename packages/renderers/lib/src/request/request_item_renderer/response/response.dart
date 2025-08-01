7c720d65a595bd6e28074109b046dad31d131500
Julian König <julian.koenig@js-soft.com>
2025-08-01 14:57:46 +0200

    chore: remove old widgets

--- packages/renderers/lib/src/request/request_item_renderer/response/response.dart
+++ packages/renderers/lib/src/request/request_item_renderer/response/response.dart
@@ -6,4 +6,3 @@ export 'read_attribute_accept_response_item_renderer.dart';
 export 'register_attribute_lister_accept_response_item_renderer.dart';
 export 'response_item_group_renderer.dart';
 export 'response_item_renderer.dart';
-export 'share_attribute_accept_response_item_renderer.dart';
