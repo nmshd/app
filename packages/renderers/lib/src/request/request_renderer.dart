f6aaf0e203f984f658978560baeb92cf1bae852c
Julian König <julian.koenig@js-soft.com>
2025-08-08 14:59:06 +0200

    chore: remove response renderers

--- packages/renderers/lib/src/request/request_renderer.dart
+++ packages/renderers/lib/src/request/request_renderer.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'open_attribute_switcher_function.dart';
 import 'request_item_group_renderer.dart';
 import 'request_item_renderer/request_item_renderer.dart';
-import 'request_item_renderer/response/response.dart';
 import 'request_renderer_controller.dart';
 
 class RequestRenderer extends StatelessWidget {
@@ -36,39 +35,6 @@ class RequestRenderer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (request.response != null) {
-      final responseItems = request.response!.content.items.mapIndexed((index, item) {
-        final itemIndex = (rootIndex: index, innerIndex: null);
-
-        if (item is ResponseItemGroupDVO) {
-          final requestItemGroup = request.items[index] as RequestItemGroupDVO;
-          return ResponseItemGroupRenderer(
-            responseItemGroup: item,
-            requestItemGroup: requestItemGroup,
-            itemIndex: itemIndex,
-            expandFileReference: expandFileReference,
-            chooseFile: chooseFile,
-            openFileDetails: openFileDetails,
-            openAttributeSwitcher: openAttributeSwitcher,
-            createAttribute: createAttribute,
-          );
-        }
-
-        return ResponseItemRenderer(
-          responseItem: item,
-          itemIndex: itemIndex,
-          requestItem: request.items[index],
-          expandFileReference: expandFileReference,
-          chooseFile: chooseFile,
-          openFileDetails: openFileDetails,
-          openAttributeSwitcher: openAttributeSwitcher,
-          createAttribute: createAttribute,
-        );
-      }).toList();
-
-      return Column(children: responseItems);
-    }
-
     final requestItems = request.items.mapIndexed((index, item) {
       final itemIndex = (rootIndex: index, innerIndex: null);
 
