0eefd5042beec6b33c59a9282454195ef0dc97a0
Jakob Erben <jakob.erben@js-soft.com>
2025-08-28 14:49:36 +0200

    feat: setting up facade

--- packages/enmeshed_runtime_bridge/lib/src/services/facades/consumption.dart
+++ packages/enmeshed_runtime_bridge/lib/src/services/facades/consumption.dart
@@ -1,5 +1,6 @@
 export 'consumption/attribute_listeners_facade.dart';
 export 'consumption/attributes_facade.dart';
 export 'consumption/incoming_requests_facade.dart';
+export 'consumption/openid4vc_facade.dart';
 export 'consumption/outgoing_requests_facade.dart';
 export 'consumption/settings_facade.dart';
