ec4c2acb30fa2b7e2cbeaea2f72337e53824667a
Julian König <julian.koenig@js-soft.com>
2025-08-28 11:25:15 +0200

    chore: remove unused import

--- packages/renderers/lib/src/request/request_item_renderer/read_attribute_request_item_renderer.dart
+++ packages/renderers/lib/src/request/request_item_renderer/read_attribute_request_item_renderer.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:enmeshed_types/enmeshed_types.dart';
 import 'package:flutter/material.dart';
 import 'package:i18n_translated_text/i18n_translated_text.dart';
