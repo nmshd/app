7cea6cc00037a552a335a40c866756888402e783
Jakob Erben <jakob.erben@js-soft.com>
2025-09-29 17:22:52 +0200

    fix: properly load and store the keys

--- packages/enmeshed_runtime_bridge/lib/src/services/facades/consumption/openid4vc_facade.dart
+++ packages/enmeshed_runtime_bridge/lib/src/services/facades/consumption/openid4vc_facade.dart
@@ -5,36 +5,66 @@ import '../abstract_evaluator.dart';
 import '../handle_call_async_js_result.dart';
 import '../utilities/utilities.dart';
 
+/*
+  Note: The keystore is managed within the apps preferences. This is a temporary solution, but this should
+  be replaced by a more secure solution before leaving the alpha stage.
+
+  To do so the keystore is stored and loaded from the SharedPreferences upon each facade call.
+*/
+
 class OpenId4VcFacade {
   final AbstractEvaluator _evaluator;
 
   OpenId4VcFacade(this._evaluator);
 
+  Future<void> loadKeyStorageFromPreferences() async {
+    final preferences = await SharedPreferences.getInstance();
+    final keyStore = preferences.getString('keyStore');
+    if (keyStore != null) {
+      await _evaluator.evaluateJavaScript(
+        '''
+        const parsedMap = new Map(JSON.parse(keyStore));
+        window.fakeKeyStorage = parsedMap;
+        ''',
+        arguments: {'keyStore': keyStore},
+      );
+    }
+  }
+
+  Future<void> storeKeyStorageToPreferences(String jsonEncodedKeyStorage) async {
+    final preferences = await SharedPreferences.getInstance();
+    await preferences.setString('keyStore', jsonEncodedKeyStorage);
+  }
+
   Future<Result<VerifiableCredentialDTO>> acceptCredentialOffer(String url) async {
+    await loadKeyStorageFromPreferences();
     final result = await _evaluator.evaluateJavaScript(
       '''
       const result = await session.consumptionServices.openId4Vc.resolveCredentialOffer(request)
       if (result.isError) return { error: { message: result.error.message, code: result.error.code } }
-      return { value: result.value }''',
+      const keyStorage = JSON.stringify(Array.from(window.fakeKeyStorage));
+      return { value: result.value , keyStorage: keyStorage}''',
       arguments: {
         'request': {'credentialOfferUrl': url},
       },
     );
-    print('VerifiableCredentialDTO data: ${result.valueToMap()['value']['data']}');
+    storeKeyStorageToPreferences(result.valueToMap()['keyStorage'].toString());
     return Result.fromJson(result.valueToMap(), (x) => VerifiableCredentialDTO.fromJson(x));
   }
 
   Future<Result<String>> fetchCredentialOffer(String url) async {
+    await loadKeyStorageFromPreferences();
     final result = await _evaluator.evaluateJavaScript(
       '''
       const result = await session.consumptionServices.openId4Vc.fetchCredentialOffer(request)
       if (result.isError) return { error: { message: result.error.message, code: result.error.code } }
-      return { value: result.value }''',
+      const keyStorage = JSON.stringify(Array.from(window.fakeKeyStorage));
+      return { value: result.value , keyStorage: keyStorage}''',
       arguments: {
         'request': {'credentialOfferUrl': url},
       },
     );
-    print(result.valueToMap());
+    storeKeyStorageToPreferences(result.valueToMap()['keyStorage'].toString());
     return Result.success(result.valueToMap()['value']['jsonRepresentation'] as String? ?? 'ERROR');
   }
 
@@ -43,6 +73,7 @@ class OpenId4VcFacade {
     String? pinCode,
     List<String> requestedCredentials,
   ) async {
+    await loadKeyStorageFromPreferences();
     if (pinCode == null) {
       final result = await _evaluator.evaluateJavaScript(
         '''
@@ -54,11 +85,7 @@ class OpenId4VcFacade {
           'request': {'data': fetchedOfferJson, 'requestedCredentials': requestedCredentials},
         },
       );
-      //--------------- START HACK PART 1- NEVER DO THIS IN PRODUCTION ----------
-      // before returning we will store the key storage within the bounds of the app
-      final preferences = await SharedPreferences.getInstance();
-      await preferences.setString('keyStore', result.valueToMap()['keyStorage'].toString());
-      //-------------- END HACK -------------------
+      storeKeyStorageToPreferences(result.valueToMap()['keyStorage'].toString());
       return Result.fromJson(result.valueToMap(), (x) => VerifiableCredentialDTO.fromJson(x));
     }
 
@@ -66,53 +93,45 @@ class OpenId4VcFacade {
       '''
       const result = await session.consumptionServices.openId4Vc.resolveFetchedCredentialOffer(request)
       if (result.isError) return { error: { message: result.error.message, code: result.error.code } }
-      return { value: result.value }''',
+      const keyStorage = JSON.stringify(Array.from(window.fakeKeyStorage));
+      return { value: result.value , keyStorage: keyStorage}''',
       arguments: {
         'request': {'data': fetchedOfferJson, 'pinCode': pinCode, 'requestedCredentials': requestedCredentials},
       },
     );
+    storeKeyStorageToPreferences(result.valueToMap()['keyStorage'].toString());
     return Result.fromJson(result.valueToMap(), (x) => VerifiableCredentialDTO.fromJson(x));
   }
 
   Future<Result<String>> fetchPresentationRequest(String url) async {
-    // --------------- START HACK PART 2 - NEVER DO THIS IN PRODUCTION ----------
-    // before fetching the presentation request we will restore the key storage to the javascript environment
-    final preferences = await SharedPreferences.getInstance();
-    final keyStore = preferences.getString('keyStore');
-    if (keyStore != null) {
-      await _evaluator.evaluateJavaScript(
-        '''
-        const parsedMap = new Map(JSON.parse(keyStore));
-        console.log('Restored key storage: ', JSON.stringify(Array.from(parsedMap)));
-        window.fakeKeyStorage = parsedMap;
-        ''',
-        arguments: {'keyStore': keyStore},
-      );
-      print('Restored key storage from app storage to JS environment.');
-    }
-
+    await loadKeyStorageFromPreferences();
     final result = await _evaluator.evaluateJavaScript(
       '''
       const result = await session.consumptionServices.openId4Vc.fetchProofRequest(request)
       if (result.isError) return { error: { message: result.error.message, code: result.error.code } }
-      return { value: result.value }''',
+      const keyStorage = JSON.stringify(Array.from(window.fakeKeyStorage));
+      return { value: result.value , keyStorage: keyStorage}''',
       arguments: {
         'request': {'proofRequestUrl': url},
       },
     );
+    storeKeyStorageToPreferences(result.valueToMap()['keyStorage'].toString());
     return Result.success(result.valueToMap()['value']['jsonRepresentation'] as String? ?? 'ERROR');
   }
 
   Future<Result<int>> acceptFetchedCredentialPresentation(String fetchedRequestJson) async {
+    await loadKeyStorageFromPreferences();
     final result = await _evaluator.evaluateJavaScript(
       '''
       const result = await session.consumptionServices.openId4Vc.acceptProofRequest(request)
       if (result.isError) return { error: { message: result.error.message, code: result.error.code } }
-      return { value: result.value }''',
+      const keyStorage = JSON.stringify(Array.from(window.fakeKeyStorage));
+      return { value: result.value , keyStorage: keyStorage}''',
       arguments: {
         'request': {'jsonEncodedRequest': fetchedRequestJson},
       },
     );
+    storeKeyStorageToPreferences(result.valueToMap()['keyStorage'].toString());
     return Result.success(result.valueToMap()['value']['status']);
   }
 }
